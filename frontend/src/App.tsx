1ee40f30e8b68cb1c36abfba844e0c7688686e9c
Ivan <ivan.kolipov@gmail.com>
2022-12-29 17:41:56 +0300

    Clean components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,8 @@
-import React, { ReactElement } from 'react'
-import './App.css'
+import React, { FC } from 'react';
+import './App.css';
 
-function App (): ReactElement {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
-}
+const App: FC = () => {
+    return <div>dfdsf</div>;
+};
 
-export default App
+export default App;

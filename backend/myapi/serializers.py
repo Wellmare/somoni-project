1659c9cf88233500c29509b9eb40ec15da5774c2
Danila <morozovd0605@gmail.com>
2022-12-30 20:03:23 +0300

    add email to register

--- backend/myapi/serializers.py
+++ backend/myapi/serializers.py
@@ -38,7 +38,8 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create(
-            username=validated_data['username']
+            username=validated_data['username'],
+            email=validated_data['email']
         )
 
         user.set_password(validated_data['password'])

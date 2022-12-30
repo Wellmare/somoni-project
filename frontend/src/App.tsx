878206fc9e567999d28cc2850fe79ec1df37b6f2
Ivan <ivan.kolipov@gmail.com>
2022-12-30 19:14:28 +0300

    test app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,29 @@
 import React, { FC } from 'react';
+
 import './App.css';
+import { useLoginUserMutation } from './redux/api';
+// import { useGetIsLoginQuery } from './redux/api';
 
 const App: FC = () => {
-    return <div>dfdsf</div>;
+    // const count = useAppSelector(selectCount);
+    // const dispatch = useAppDispatch();
+
+    // const addCount = (count: number) => ;
+
+    // const data = useGetIsLoginQuery(undefined);
+    // console.log(data);
+
+    const [login, { isLoading, data }] = useLoginUserMutation({ username: 'test', password: 'test' });
+    console.log(login);
+    console.log(isLoading);
+    console.log(data);
+
+    return (
+        <>
+            <div>1</div>
+            <button>add count</button>
+        </>
+    );
 };
 
 export default App;

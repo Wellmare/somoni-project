bf5d34b4f51aecdf581345c60b2edb65a27e8e73
Ivan <ivan.kolipov@gmail.com>
2023-01-01 20:10:54 +0300

    Add simple logic login, register and private path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,59 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 
 import './App.css';
-import { useLoginUserMutation } from './redux/api';
-// import { useGetIsLoginQuery } from './redux/api';
+import { AuthContext } from './context/authContext';
+import useWithAuth from './hooks/useWithAuth';
+import { doAsyncFunc } from './utils/doAsyncFunc';
+// import Input from './components/Input/Input';
 
 const App: FC = () => {
-    // const count = useAppSelector(selectCount);
-    // const dispatch = useAppDispatch();
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const { loginUser, registerUser, logoutUser, errorCode, errorMessage } = useContext(AuthContext)!;
+    const req = useWithAuth();
 
-    // const addCount = (count: number) => ;
+    const onRegister = (): void => {
+        doAsyncFunc(async () => {
+            await registerUser({
+                username: 'username1',
+                password: 'test12345',
+                password2: 'test12345',
+                email: 'test@gmail.com',
+            });
+        });
+    };
 
-    // const data = useGetIsLoginQuery(undefined);
-    // console.log(data);
+    const onLogin = (): void => {
+        doAsyncFunc(async () => {
+            await loginUser({
+                username: 'username1',
+                password: 'test12345',
+            });
+        });
+    };
 
-    const [login, { isLoading, data }] = useLoginUserMutation({ username: 'test', password: 'test' });
-    console.log(login);
-    console.log(isLoading);
-    console.log(data);
+    const onLogout = (): void => {
+        logoutUser();
+    };
+
+    const onPrivatePage = (): void => {
+        doAsyncFunc(async () => {
+            const res = await req.get('test');
+            console.log(res);
+        });
+    };
+
+    // useEffect(() => {
+    //
+    // }, [errorCode])
 
     return (
         <>
-            <div>1</div>
-            <button>add count</button>
+            {errorCode !== 200 ? errorCode : ''}
+            {errorMessage}
+            <button onClick={onRegister}>REGISTER</button>
+            <button onClick={onLogin}>LOGIN</button>
+            <button onClick={onLogout}>LOGOUT</button>
+            <button onClick={onPrivatePage}>PRIVATE</button>
         </>
     );
 };

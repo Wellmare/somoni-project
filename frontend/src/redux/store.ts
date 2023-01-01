122369be3e7193f3000961c8317a68001bc86e19
Ivan <ivan.kolipov@gmail.com>
2023-01-01 20:07:35 +0300

    Remove store

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,18 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import api from './api';
+// import api from '../service/api';
 
-// const rootReducer = combineReducers([testReducer]);
-
-const store = configureStore({
-    reducer: { [api.reducerPath]: api.reducer },
-    devTools: true,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
-});
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
-
-export default store;
+// const store = configureStore({
+//     reducer: { [api.reducerPath]: api.reducer },
+//     devTools: true,
+//     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
+// });
+//
+// // Infer the `RootState` and `AppDispatch` types from the store itself
+// export type RootState = ReturnType<typeof store.getState>;
+// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+// export type AppDispatch = typeof store.dispatch;
+//
+// export default store;

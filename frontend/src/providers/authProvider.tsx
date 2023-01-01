5f7a7fc111e598d0d90b0518615c84c6b21f84b8
Ivan <ivan.kolipov@gmail.com>
2023-01-01 20:10:15 +0300

    Enhance AuthProvider

--- frontend/src/providers/authProvider.tsx
+++ frontend/src/providers/authProvider.tsx
@@ -1,67 +1,122 @@
-import { AxiosResponse } from 'axios';
+import { AxiosError } from 'axios';
 import jwtDecode from 'jwt-decode';
 import React, { FC, ReactNode, useEffect, useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
-import { apiEndpoints } from '../constants/apiEndpoints';
 import { AuthContext } from '../context/authContext';
-import { baseAxiosInstance } from '../service/api';
+import { loginApi } from '../service/login.api';
 import { IAuthContext, IDataToLogin, IDataToRegister, IUserJWTDecodeResponse, ITokens } from '../types/login.types';
 
-interface props {
+interface AuthProviderProps {
     children: ReactNode;
 }
 
-export const AuthProvider: FC<props> = ({ children }) => {
+export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
+    // AUTH TOKENS -->
     const authTokensFromLocalStorage: null | string = localStorage.getItem('authTokens');
     const parsedAuthTokes: null | { access: string; refresh: string } =
         authTokensFromLocalStorage !== null ? JSON.parse(authTokensFromLocalStorage) : null;
+    // <-- AUTH TOKENS
 
+    // STATE -->
     const [authTokens, setAuthTokens] = useState<ITokens | null>(parsedAuthTokes);
 
     const [user, setUser] = useState<IUserJWTDecodeResponse | null>(() =>
         parsedAuthTokes != null ? jwtDecode(parsedAuthTokes.access) : null,
     );
     const [loading, setLoading] = useState<boolean>(true);
+    const [errorCode, setErrorCode] = useState<number>(200);
+    const [errorMessage, setErrorMessage] = useState<string>('');
+    // <-- STATE
 
     const navigate = useNavigate();
-    // const history = useHistory();
 
+    // FUNCTIONS -->
     const loginUser = async ({ username, password }: IDataToLogin): Promise<void> => {
-        const response = await baseAxiosInstance.post<IDataToLogin, AxiosResponse<ITokens>>(
-            apiEndpoints.login,
-            {
+        // const response = await loginApi.login({
+        //     username,
+        //     password,
+        // });
+        // const data = response.data;
+        //
+        // if (response.status === 200) {
+        //     setAuthTokens(data);
+        //     setUser(jwtDecode(data.access));
+        //     localStorage.setItem('authTokens', JSON.stringify(data));
+        //     navigate('/');
+        //     // return {
+        //     //     access: response.data.access,
+        //     //     refresh: response.data.refresh,
+        //     // };
+        // } else {
+        //     setErrorCode(response.status);
+        // }
+        loginApi
+            .login({
                 username,
                 password,
-            },
-            {},
-        );
+            })
+            .then((response) => {
+                if (response.status === 200) {
+                    setAuthTokens(response.data);
+                    setUser(jwtDecode(response.data.access));
+                    localStorage.setItem('authTokens', JSON.stringify(response.data));
+                    navigate('/');
 
-        const data = response.data;
-
-        if (response.status === 200) {
-            setAuthTokens(data);
-            setUser(jwtDecode(data.access));
-            localStorage.setItem('authTokens', JSON.stringify(data));
-            navigate('/');
-        } else {
-            alert('Something went wrong!');
-        }
+                    console.log(response.data);
+                    setErrorMessage('');
+                    setErrorCode(200);
+                }
+            })
+            .catch((e: AxiosError) => {
+                console.warn(e);
+                if (e.response != null) {
+                    setErrorCode(e.response.status);
+                    setErrorMessage(e.response.statusText);
+                }
+            });
     };
 
     const registerUser = async ({ username, password, password2, email }: IDataToRegister): Promise<void> => {
-        const response = await baseAxiosInstance.post(apiEndpoints.register, {
-            username,
-            password,
-            password2,
-            email,
-        });
-        if (response.status === 201) {
-            navigate('/login');
-        } else {
-            alert('Something went wrong!');
-        }
+        // const response = await loginApi.register({
+        //     username,
+        //     password,
+        //     password2,
+        //     email,
+        // });
+        loginApi
+            .register({
+                username,
+                password,
+                password2,
+                email,
+            })
+            .then((response) => {
+                if (response.status === 201) {
+                    navigate('/login');
+                    setErrorMessage('');
+                    setErrorCode(200);
+                }
+            })
+            .catch((e: AxiosError) => {
+                console.warn(e);
+                if (e.response != null) {
+                    setErrorCode(e.response.status);
+                    setErrorMessage(e.response.statusText);
+                }
+            });
+
+        // console.log('reg');
+        // if (response.status === 201) {
+        //     navigate('/login');
+        //     // return { username: response.data.username, email: response.data.email };
+        // } else {
+        //     // return {
+        //     //     code: response.status,
+        //     // };
+        //     setErrorCode(response.status);
+        // }
     };
 
     const logoutUser = (): void => {
@@ -70,6 +125,7 @@ export const AuthProvider: FC<props> = ({ children }) => {
         localStorage.removeItem('authTokens');
         navigate('/');
     };
+    // <-- FUNCTIONS
 
     const contextData: IAuthContext = {
         user,
@@ -79,6 +135,9 @@ export const AuthProvider: FC<props> = ({ children }) => {
         registerUser,
         loginUser,
         logoutUser,
+        loading,
+        errorCode,
+        errorMessage,
     };
 
     useEffect(() => {

fec2fb6e5d0a20587bc14a7a79df70b343247a18
Ivan <ivan.kolipov@gmail.com>
2023-01-01 14:21:35 +0300

    Add hook with useWithAuth

---
+++ frontend/src/hooks/useWithAuth.ts
@@ -0,0 +1,47 @@
+import axios, { AxiosInstance } from 'axios';
+
+import dayjs from 'dayjs';
+import jwtDecode from 'jwt-decode';
+import { useContext } from 'react';
+
+import { url } from '../constants/api';
+import { apiEndpoints } from '../constants/apiEndpoints';
+import { AuthContext } from '../context/authContext';
+import { baseAxiosInstance } from '../service/api';
+import { IUserJWTDecodeResponse, ITokens } from '../types/login.types';
+
+export const useWithAuth = (): AxiosInstance => {
+    const context = useContext(AuthContext);
+    if (AuthContext === null || context === null || context.authTokens === null) return baseAxiosInstance;
+    const { setUser, authTokens, setAuthTokens } = context;
+
+    const axiosInstance = axios.create({
+        baseURL: url,
+        headers: { Authorization: `Bearer ${authTokens.access}` },
+    });
+
+    axiosInstance.interceptors.request.use(async (req) => {
+        const user: IUserJWTDecodeResponse = jwtDecode(authTokens.access);
+        const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+
+        if (!isExpired) return req;
+
+        const response = await axios.post<ITokens>(apiEndpoints.refreshToken, {
+            refresh: authTokens.refresh,
+        });
+
+        localStorage.setItem('authTokens', JSON.stringify(response.data));
+
+        setAuthTokens(response.data);
+        setUser(jwtDecode(response.data.access));
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        req.headers.Authorization = `Bearer ${response.data.access}`;
+        return req;
+    });
+
+    return axiosInstance;
+};
+
+export default useWithAuth;

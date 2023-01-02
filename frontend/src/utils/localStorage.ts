de9b0e9f38bb91c653094ff8307e6e0ffe169ee6
Ivan <ivan.kolipov@gmail.com>
2023-01-02 13:18:42 +0300

    Add localStorage methods

---
+++ frontend/src/utils/localStorage.ts
@@ -0,0 +1,12 @@
+import { localStorageKeys } from '../types/localStorageKeys';
+
+export const getDataFromLocalStorage = <T>(key: localStorageKeys): T | null => {
+    const value = localStorage.getItem(key);
+    if (value != null) return JSON.parse(value) as T;
+    return null;
+};
+
+export const setDataToLocalStorage = <T>(key: localStorageKeys, value: T): void => {
+    const json = JSON.stringify(value);
+    localStorage.setItem(key, json);
+};

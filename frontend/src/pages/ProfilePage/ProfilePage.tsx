b8a90068d5a7fcb764efa171871e511cfef6d620
Ivan <ivan.kolipov@gmail.com>
2023-01-02 16:39:04 +0300

    Add simple params id

--- frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,7 +1,13 @@
 import React, { FC } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ProfilePage: FC = () => {
-    return <div></div>;
+    const params = useParams();
+    console.log(params);
+
+    console.log(params.id);
+
+    return <div>{params.id !== undefined ? +params.id : 'My profile'}</div>;
 };
 
 export default ProfilePage;

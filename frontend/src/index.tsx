faadc9da131bb937074f3b2713c36c6c070d2bb2
Ivan <ivan.kolipov@gmail.com>
2023-01-02 13:20:34 +0300

    Remove styles from App and index.tsx Add import global.css to index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
-
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { AuthProvider } from './providers/authProvider';
+import ThemeProvider from './providers/ThemeProvider';
+
+import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     // <Provider store={store}>
     <BrowserRouter>
-        <AuthProvider>
-            <App />
-        </AuthProvider>
-        ,
+        <ThemeProvider>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </ThemeProvider>
     </BrowserRouter>,
     // </Provider>,
 );

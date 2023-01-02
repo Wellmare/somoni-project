bf8c840e265d84a2bd8c085aca7bbadb14588a6a
Ivan <ivan.kolipov@gmail.com>
2023-01-02 14:12:54 +0300

    Add routing through the pages

--- frontend/src/components/Routing/Routing.tsx
+++ frontend/src/components/Routing/Routing.tsx
@@ -1,14 +1,31 @@
 import React, { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import {
+    ChangePasswordPage,
+    LoginPage,
+    MainPage,
+    PostPage,
+    ProfilePage,
+    RegisterPage,
+    RestorePasswordPage,
+    WelcomePage,
+} from '../../pages';
 import Layout from '../Layout/Layout';
-import Test from '../Test/Test';
 
 const Routing: FC = () => {
     return (
         <Routes>
             <Route path={'/'} element={<Layout />}>
-                <Route path={'/login'} element={<Test />} />
+                <Route path={''} element={<WelcomePage />} />
+                <Route path={'main'} element={<MainPage />} />
+                <Route path={'login'} element={<LoginPage />} />
+                <Route path={'register'} element={<RegisterPage />} />
+                <Route path={'post/:id'} element={<PostPage />} />
+                <Route path={'profile'} element={<ProfilePage />} />
+                <Route path={'profile/:id'} element={<ProfilePage />} />
+                <Route path={'change-password'} element={<ChangePasswordPage />} />
+                <Route path={'restore-password'} element={<RestorePasswordPage />} />
             </Route>
         </Routes>
     );

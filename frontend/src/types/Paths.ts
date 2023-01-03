5b645f0cc1f5536f112cfb70b4e31e9068874e1b
Ivan <ivan.kolipov@gmail.com>
2023-01-03 17:16:55 +0300

    Add paths enum

---
+++ frontend/src/types/Paths.ts
@@ -0,0 +1,10 @@
+export const enum Paths {
+    WELCOME = '',
+    MAIN = 'main',
+    LOGIN = 'login',
+    REGISTER = 'register',
+    POST = 'post',
+    PROFILE = 'profile',
+    CHANGE_PASSWORD = 'change-password',
+    RESTORE_PASSWORD = 'restore-password',
+}

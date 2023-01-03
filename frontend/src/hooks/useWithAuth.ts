5f6cc76830136239b9ec55ee1f5f9346d42c440d
Ivan <ivan.kolipov@gmail.com>
2023-01-03 16:25:54 +0300

    Rewrite hook useWithAuth

--- frontend/src/hooks/useWithAuth.ts
+++ frontend/src/hooks/useWithAuth.ts
@@ -1,50 +1,43 @@
 import axios, { AxiosInstance } from 'axios';
 
 import dayjs from 'dayjs';
-import jwtDecode from 'jwt-decode';
-import { useContext } from 'react';
+
+import { useAppDispatch, useAppSelector } from './reduxHooks';
 
 import { url } from '../constants/api';
-import { AuthContext } from '../context/authContext';
+import { setAuthTokens } from '../redux/slices/authSlice';
 import { baseAxiosInstance } from '../service/api';
 import { loginApi } from '../service/login.api';
-import { IUserJWTDecodeResponse } from '../types/login.types';
+// import { IUserJWTDecodeResponse } from '../types/login.types';
 
 export const useWithAuth = (): [boolean, AxiosInstance] => {
-    const context = useContext(AuthContext);
-    if (AuthContext === null || context === null || context.authTokens === null) return [false, baseAxiosInstance];
-    const { setUser, authTokens, setAuthTokens } = context;
+    const dispatch = useAppDispatch();
+    const { isAuth, authTokens, user } = useAppSelector((state) => state.auth);
+
+    if (!isAuth || authTokens === null || user == null) return [false, baseAxiosInstance];
 
     const axiosInstance = axios.create({
         baseURL: url,
-        headers: { Authorization: `Bearer ${authTokens?.access}`, 'Content-type': 'application/json' },
+        headers: { Authorization: `Bearer ${authTokens.access}`, 'Content-type': 'application/json' },
     });
 
-    axiosInstance.interceptors.request.use(
-        async (req) => {
-            const user: IUserJWTDecodeResponse = jwtDecode(authTokens.access);
-            const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+    axiosInstance.interceptors.request.use(async (req) => {
+        // const user: IUserJWTDecodeResponse = jwtDecode(authTokens.access);
+        const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+        if (!isExpired) return req;
 
-            if (!isExpired) return req;
-            try {
-                const response = await loginApi.refreshToken(authTokens?.refresh);
-                localStorage.setItem('authTokens', JSON.stringify(response.data));
+        try {
+            const response = await loginApi.refreshToken(authTokens.refresh);
+            dispatch(setAuthTokens(response.data));
 
-                setAuthTokens(response.data);
-                setUser(jwtDecode(response.data.access));
-
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-expect-error
-                req.headers.Authorization = `Bearer ${response.data.access}`;
-            } catch (e) {
-                console.log(e);
-            }
-            return req;
-        },
-        (error) => {
-            console.log(error);
-        },
-    );
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            req.headers.Authorization = `Bearer ${response.data.access}`;
+        } catch (e) {
+            console.log(e);
+        }
+        return req;
+    });
 
     return [true, axiosInstance];
 };

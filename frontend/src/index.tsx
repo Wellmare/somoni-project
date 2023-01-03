01a87f9dd2eae9d10d5aaa31dde709399b9c82be
Ivan <ivan.kolipov@gmail.com>
2023-01-03 16:22:49 +0300

    Add store to index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
@@ -8,16 +9,17 @@ import { AuthProvider } from './providers/authProvider';
 import ThemeProvider from './providers/ThemeProvider';
 
 import './styles/global.css';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    // <Provider store={store}>
-    <BrowserRouter>
-        <ThemeProvider>
-            <AuthProvider>
-                <App />
-            </AuthProvider>
-        </ThemeProvider>
-    </BrowserRouter>,
-    // </Provider>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <ThemeProvider>
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            </ThemeProvider>
+        </BrowserRouter>
+    </Provider>,
 );

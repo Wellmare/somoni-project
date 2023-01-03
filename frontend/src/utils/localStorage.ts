f0b16b6dba0cc65c7a84a430f28b8b8eff2deb57
Ivan <ivan.kolipov@gmail.com>
2023-01-03 16:20:26 +0300

    Add func removeDataFromLocalStorage

--- frontend/src/utils/localStorage.ts
+++ frontend/src/utils/localStorage.ts
@@ -10,3 +10,7 @@ export const setDataToLocalStorage = <T>(key: localStorageKeys, value: T): void 
     const json = JSON.stringify(value);
     localStorage.setItem(key, json);
 };
+
+export const removeDataFromLocalStorage = (key: localStorageKeys): void => {
+    localStorage.removeItem(key);
+};

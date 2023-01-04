51267cd5d411e1ab98d555e63529dd35ffd10740
Ivan <ivan.kolipov@gmail.com>
2023-01-04 17:13:49 +0300

    Add apiSlice to store

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -2,13 +2,17 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import authSlice from './slices/authSlice';
 
+import { apiSlice } from '../service/apiSlice';
+
 const rootReducer = combineReducers({
     auth: authSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer,
 });
 
 const store = configureStore({
     reducer: rootReducer,
     devTools: true,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself

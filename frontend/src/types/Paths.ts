6956181ee7358ae28abfa4dbff8be684fced0f47
Ivan <ivan.kolipov@gmail.com>
2023-01-06 10:50:00 +0300

    Add enum PathsToNavigate

--- frontend/src/types/Paths.ts
+++ frontend/src/types/Paths.ts
@@ -8,3 +8,14 @@ export const enum Paths {
     CHANGE_PASSWORD = 'change-password',
     RESTORE_PASSWORD = 'restore-password',
 }
+
+export const enum PathsToNavigate {
+    WELCOME = '/',
+    MAIN = '/main',
+    LOGIN = '/login',
+    REGISTER = '/register',
+    POST = '/post',
+    PROFILE = '/profile',
+    CHANGE_PASSWORD = '/change-password',
+    RESTORE_PASSWORD = '/restore-password',
+}

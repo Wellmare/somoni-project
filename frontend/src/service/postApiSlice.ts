c68f8f468c9740df13660f438caae1619a2f841b
Ivan <ivan.kolipov@gmail.com>
2023-01-06 13:49:02 +0300

    Add postApiSlice and types

---
+++ frontend/src/service/postApiSlice.ts
@@ -0,0 +1,32 @@
+import { apiSlice } from './index';
+
+import { apiEndpoints } from '../constants/apiEndpoints';
+import { IDataToGetPost, IDataToLikePost } from '../types/redux/postSlice.types';
+import { IPost } from '../types/redux/postsSlice.types';
+
+export const postApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getPost: builder.query<IPost, IDataToGetPost>({
+            query: ({ id }) => ({
+                url: `${apiEndpoints.post}${id}`,
+            }),
+            providesTags: (result) => [{ type: 'Single Post', id: result?.id }],
+        }),
+        likePost: builder.mutation<undefined, IDataToLikePost>({
+            query: ({ id }) => ({
+                url: `${apiEndpoints.post}${id}/like`,
+                method: 'POST',
+            }),
+            invalidatesTags: (result, error, arg) => [{ type: 'Single Post', id: arg.id }],
+        }),
+        unlikePost: builder.mutation<undefined, IDataToLikePost>({
+            query: ({ id }) => ({
+                url: `${apiEndpoints.post}${id}/like`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: (result, error, arg) => [{ type: 'Single Post', id: arg.id }],
+        }),
+    }),
+});
+
+export const { useGetPostQuery, useLikePostMutation, useUnlikePostMutation } = postApiSlice;

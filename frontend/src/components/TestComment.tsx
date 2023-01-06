0b9338f08e8f39698ce660699aaf4f243e2336e9
Ivan <ivan.kolipov@gmail.com>
2023-01-06 13:50:42 +0300

    Add test api components

---
+++ frontend/src/components/TestComment.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+
+import { IComment } from '../types/redux/commentsSlice.types';
+
+interface ITestCommentProps {
+    comment: IComment;
+}
+
+const TestComment: FC<ITestCommentProps> = ({ comment: { id, date, content, author } }) => {
+    return (
+        <div style={{ display: 'flex' }}>
+            <p>id: {id}</p>
+            <p>Date: {new Date(date).toLocaleDateString()}</p>
+            <p>Content: {content}</p>
+            <p>Author: {author}</p>
+        </div>
+    );
+};
+
+export default TestComment;
--- frontend/src/components/TestPost.tsx
+++ frontend/src/components/TestPost.tsx
@@ -1,27 +1,50 @@
 import React, { FC } from 'react';
 
+import { useLikePostMutation, useUnlikePostMutation } from '../service/postApiSlice';
 import { IPost } from '../types/redux/postsSlice.types';
+import { doAsyncFunc } from '../utils/doAsyncFunc';
 
 interface ITestPostProps {
     post: IPost;
 }
 
 const TestPost: FC<ITestPostProps> = ({ post }) => {
-    const { content, date, id, image, likes, title, comments, author } = post;
+    const { content, date, id, image, likes, title, comments, author, isLiked, username } = post;
+    const [like] = useLikePostMutation();
+    const [unlike] = useUnlikePostMutation();
+
+    const onLike = (): void => {
+        doAsyncFunc(async () => {
+            // const response = await turnLike({ id: id.toString() });\
+            if (isLiked) {
+                await unlike({ id: id.toString() });
+            } else {
+                await like({ id: id.toString() });
+            }
+        });
+    };
+
     return (
-        <div>
-            <p>Date: {date}</p>
-            <p>content: {content}</p>
-            <p>id: {id}</p>
-            {image != null && (
-                <p>
-                    image: <img src={image} alt='image' style={{ maxWidth: '200px', maxHeight: '200px' }} />
-                </p>
-            )}
-            <p>Likes: {likes}</p>
-            <p>title: {title}</p>
-            <p>comments: {comments}</p>
-            <p>author: {author}</p>
+        <div className='p-5' style={{ padding: '1rem', margin: '1rem' }}>
+            {image != null && <img src={image} alt='post img' className={'max-h-72'} style={{ maxHeight: '300px' }} />}
+            <p>Username: {username}</p>
+            <p>Date: {new Date(date).toLocaleDateString()}</p>
+            <p>Title: {title}</p>
+            <p>Content: {content}</p>
+            <p>isLiked: {Number(isLiked)}</p>
+            <button onClick={onLike}>Like</button>
+            {/* <p>Date: {date}</p> */}
+            {/* <p>content: {content}</p> */}
+            {/* <p>id: {id}</p> */}
+            {/* {image != null && ( */}
+            {/*     <p> */}
+            {/*         image: <img src={image} alt='image' style={{ maxWidth: '200px', maxHeight: '200px' }} /> */}
+            {/*     </p> */}
+            {/* )} */}
+            {/* <p>Likes: {likes}</p> */}
+            {/* <p>title: {title}</p> */}
+            {/* <p>comments: {comments}</p> */}
+            {/* <p>author: {author}</p> */}
         </div>
     );
 };

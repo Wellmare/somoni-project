d2b4452c04d4693097896745309a12446f77e213
Ivan <ivan.kolipov@gmail.com>
2023-01-07 18:15:38 +0300

    Remove tests components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
 import React, { FC } from 'react';
 
 import Routing from './components/Routing/Routing';
-import TestLogin from './components/TestLogin';
 
 const App: FC = () => {
     return (
         <>
-            <TestLogin />
-            {/* <TestFormCreatePost /> */}
-            {/* <TestPosts /> */}
             <Routing />
         </>
     );

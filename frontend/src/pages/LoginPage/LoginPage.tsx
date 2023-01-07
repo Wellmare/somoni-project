07944038cf96ccbc0fab99907462e41f9906e76a
Ivan <ivan.kolipov@gmail.com>
2023-01-07 11:42:02 +0300

    Prototype login

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,16 @@
+import classNames from 'classnames';
 import React, { FC } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
+import { useNavigate } from 'react-router-dom';
+
 import FormInput from '../../components/FormInput/FormInput';
+import { useAppDispatch } from '../../hooks/reduxHooks';
+import { setAuthTokens } from '../../redux/slices/authSlice';
+import { useLoginMutation } from '../../service/authApiSlice';
+import { PathsToNavigate } from '../../types/Paths';
+import { IDataToLogin } from '../../types/redux/auth/IDataTo';
+import { doAsyncFunc } from '../../utils/doAsyncFunc';
 
 export interface LoginPageInputs {
     username: string;
@@ -15,35 +24,55 @@ const LoginPage: FC = () => {
         formState: { errors },
     } = useForm<LoginPageInputs>({ mode: 'onBlur' });
 
+    const [login, { error, isError, data, isLoading, isSuccess }] = useLoginMutation();
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
     const onSubmit: SubmitHandler<LoginPageInputs> = (data): void => {
-        console.log('submit');
-        console.log(data.username);
-        console.log(data.password);
+        console.log('login');
+        loginUser({ username: data.username, password: data.password });
+    };
+
+    const loginUser = ({ username, password }: IDataToLogin): void => {
+        doAsyncFunc(async () => {
+            try {
+                const response = await login({ username, password }).unwrap();
+                dispatch(setAuthTokens(response));
+                navigate(PathsToNavigate.MAIN);
+            } catch (e) {
+                console.log(e);
+            }
+        });
     };
 
     return (
-        <>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <FormInput
-                    id={'username'}
-                    error={errors.username}
-                    register={() => register('username', { required: { value: true, message: 'Поле обязательно' } })}
-                    label={'Username'}
-                />
-                <FormInput
-                    id={'password'}
-                    error={errors.password}
-                    register={() =>
-                        register('password', {
-                            required: { value: true, message: 'Поле обязательно' },
-                            maxLength: { value: 255, message: 'Длина пароля должна быть меньше 255 символов' },
-                        })
-                    }
-                    label={'Password'}
-                />
-                <input style={{ display: 'block' }} type='submit' />
-            </form>
-        </>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <FormInput
+                id={'username'}
+                error={errors.username}
+                register={() => register('username', { required: { value: true, message: 'Поле обязательно' } })}
+                label={'Username'}
+            />
+            <FormInput
+                id={'password'}
+                error={errors.password}
+                register={() =>
+                    register('password', {
+                        required: { value: true, message: 'Поле обязательно' },
+                        maxLength: { value: 150, message: 'Длина пароля должна быть меньше 150 символов' },
+                    })
+                }
+                label={'Password'}
+            />
+
+            <p>{isLoading && 'Загрузка...'}</p>
+            <p>{isError && 'Неверный логин или пароль'}</p>
+
+            {/* {error} */}
+            <button className={classNames('bg-cyan-700', 'py-1', 'px-4', 'rounded-md')} type='submit'>
+                Submit
+            </button>
+        </form>
     );
 };
 

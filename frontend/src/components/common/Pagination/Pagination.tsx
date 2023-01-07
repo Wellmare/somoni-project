6cbe3644aea20a1ab57937ce84d89f34b28af4c8
Ivan <ivan.kolipov@gmail.com>
2023-01-07 18:16:07 +0300

    Add pagination component

---
+++ frontend/src/components/common/Pagination/Pagination.tsx
@@ -0,0 +1,41 @@
+import classNames from 'classnames';
+import React, { FC } from 'react';
+import ReactPaginate from 'react-paginate';
+
+import s from './Pagination.module.scss';
+
+interface IPaginationProps {
+    countPages: number;
+    handlePageChange: (page: { selected: number }) => void;
+}
+
+const Pagination: FC<IPaginationProps> = ({ handlePageChange, countPages }) => {
+    return (
+        <ReactPaginate
+            pageCount={countPages}
+            onPageChange={handlePageChange}
+            // pageRangeDisplayed={3}
+            // marginPagesDisplayed={2}
+            previousLabel={'<'}
+            nextLabel={'>'}
+            //
+            pageClassName={s.pageItem}
+            pageLinkClassName={s.pageLink}
+            previousClassName={classNames(s.pageItem, s.buttons)}
+            previousLinkClassName={s.pageLink}
+            nextClassName={classNames(s.pageItem, s.buttons)}
+            nextLinkClassName={s.pageLink}
+            //
+            breakLabel='...'
+            breakClassName={s.pageItem}
+            breakLinkClassName={s.pageLink}
+            //
+            containerClassName={s.pagination}
+            //
+            activeClassName={s.active}
+            renderOnZeroPageCount={undefined}
+        />
+    );
+};
+
+export default Pagination;

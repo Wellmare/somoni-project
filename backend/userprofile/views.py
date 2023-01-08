f0a21b2ae43ec39d242a1dbb4c926c52cad2ce5a
Danila <morozovd0605@gmail.com>
2023-01-08 16:35:55 +0300

    profile

--- backend/userprofile/views.py
+++ backend/userprofile/views.py
@@ -61,3 +61,46 @@ class profile(generics.ListCreateAPIView):
 
     def create(self, request, *args, **kwargs):
         return Response({'detail': 'method POST not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+class edit_profile(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.none()
+    serializer_class = None
+
+    def get_queryset(self):
+        return self.request.user
+
+    def get_object(self):
+        obj = get_object_or_404(User, id=self.request.user)
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user == instance.author or request.user.is_superuser:
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({'зарегайся сначала, либо ты не автор поста'}, status=status.HTTP_200_OK)
+
+    def perform_destroy(self, instance):
+        instance.delete()

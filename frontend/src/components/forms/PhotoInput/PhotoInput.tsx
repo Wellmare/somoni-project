9b430ff76b84849fcc9b017744ad88d2f16df756
Ivan <ivan.kolipov@gmail.com>
2023-01-08 16:39:16 +0300

    Remove comments

--- frontend/src/components/forms/PhotoInput/PhotoInput.tsx
+++ frontend/src/components/forms/PhotoInput/PhotoInput.tsx
@@ -4,33 +4,16 @@ import React, { FC } from 'react';
 import s from './PhotoInput.module.scss';
 
 interface IPhotoInputProps {
-    // id: string;
-    // error: FieldError | undefined;
-    // register: () => UseFormRegisterReturn;
     image: string | null;
     openFilePicker: () => void;
 }
 
 const PhotoInput: FC<IPhotoInputProps> = ({ openFilePicker, image }) => {
-    // const [isError, setIsError] = useState<boolean>(false);
-    // useEffect(() => {
-    //     setIsError(error?.message !== undefined);
-    // }, [error]);
-
     return (
         <div className={classNames('mb-3')}>
             <div className={s.avatar} onClick={() => openFilePicker()}>
                 {image !== null && <img src={image} alt='' className={s.img} />}
             </div>
-            {/* <input */}
-            {/*     type='file' */}
-            {/*     accept={'image/*'} */}
-            {/*     id={id} */}
-            {/*     {...register()} */}
-            {/*     {...props} */}
-            {/*     className={classNames('py-1', 'px-3', 'mt-1', 'rounded-md', s.hide)} */}
-            {/* /> */}
-            {/* {isError && <p className={classNames(s.error)}>{error?.message}</p>} */}
         </div>
     );
 };

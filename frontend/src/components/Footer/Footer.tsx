b2ba7ae3ea11839217fdf72c2b9d02805696d36a
Ivan <ivan.kolipov@gmail.com>
2023-01-08 11:57:59 +0300

    Add footer

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import classNames from 'classnames';
+import React, { FC } from 'react';
+
+const Footer: FC = () => {
+    return <footer className={classNames('p-5', 'w-full', 'mt-24', 'bg-gray-600')}>footer</footer>;
+};
+
+export default Footer;

209067940209da6439fabb87cc29c2280b8650e7
Ivan <ivan.kolipov@gmail.com>
2023-01-09 17:59:11 +0300

    Add useLogout

---
+++ frontend/src/hooks/useLogout.ts
@@ -0,0 +1,38 @@
+import { useAppDispatch, useAppSelector } from './reduxHooks';
+
+import { logout as logoutState } from '../redux/slices/authSlice';
+import { useLogoutMutation } from '../service/authApiSlice';
+import { doAsyncFunc } from '../utils/doAsyncFunc';
+
+interface IUseLogoutResponse {
+    logoutUser: (data?: { refresh: string }) => void;
+    isError: boolean;
+    isSuccess: boolean;
+}
+
+export const useLogout = (): IUseLogoutResponse => {
+    const refreshTokenFromState = useAppSelector((state) => state.auth.authTokens?.refresh);
+    const dispatch = useAppDispatch();
+    const [logout, { error, data, isError, isSuccess }] = useLogoutMutation();
+
+    const logoutUser = (data?: { refresh: string }): void => {
+        doAsyncFunc(async () => {
+            try {
+                if (data?.refresh !== undefined) {
+                    await logout({ refresh: data.refresh }).unwrap();
+                } else if (refreshTokenFromState !== undefined) {
+                    await logout({ refresh: refreshTokenFromState }).unwrap();
+                }
+                dispatch(logoutState());
+            } catch (e) {
+                logoutState();
+            }
+        });
+    };
+
+    return {
+        logoutUser,
+        isError,
+        isSuccess,
+    };
+};

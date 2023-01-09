fadc85a67b93e09a960ccccdcef0fe208b452019
Ivan <ivan.kolipov@gmail.com>
2023-01-09 18:44:57 +0300

    Add userApiSlice with getUser

---
+++ frontend/src/service/userApiSlice.ts
@@ -0,0 +1,37 @@
+import { apiSlice } from './index';
+
+import { apiEndpoints } from '../constants/apiEndpoints';
+import { IDataToGetProfile } from '../types/redux/profile/IDataTo';
+import { IProfile } from '../types/redux/profile/IProfile';
+
+export const userApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getUser: builder.query<IProfile, IDataToGetProfile>({
+            query: ({ userId, postsPage = 1 }) => ({
+                url: `${apiEndpoints.user}${userId}`,
+                params: {
+                    page: postsPage,
+                },
+            }),
+            providesTags: (result, error, arg) => [{ type: 'Profile', id: arg.userId }],
+        }),
+
+        // editPost: builder.mutation<IPost, IDataToEditPost>({
+        //     query: ({ formData, id }) => ({
+        //         url: `${apiEndpoints.post}${id}`,
+        //         method: 'PUT',
+        //         body: formData,
+        //     }),
+        //     invalidatesTags: (result, error, arg) => [{ type: 'Post', id: arg.id }],
+        // }),
+        // deletePost: builder.mutation<IPost, IDataToDelete>({
+        //     query: ({ id }) => ({
+        //         url: `${apiEndpoints.post}${id}`,
+        //         method: 'DELETE',
+        //     }),
+        //     invalidatesTags: (result, error, arg) => [{ type: 'Post', id: arg.id }],
+        // }),
+    }),
+});
+
+export const { useGetUserQuery } = userApiSlice;

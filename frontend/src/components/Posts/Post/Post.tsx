505ce11465d0207e21ece01e387aec5d3bf7a9c5
Ivan <ivan.kolipov@gmail.com>
2023-01-11 16:51:47 +0300

    Add Content to Post

--- frontend/src/components/Posts/Post/Post.tsx
+++ frontend/src/components/Posts/Post/Post.tsx
@@ -7,15 +7,18 @@ import s from './Post.module.scss';
 
 import { PathsToNavigate } from '../../../types/Paths';
 import { IPost } from '../../../types/redux/posts/IPost';
+import Content from '../Content/Content';
 
 interface ITestPostProps {
     post: IPost;
 }
 
 const Post: FC<ITestPostProps> = ({ post }) => {
-    const { content, username, title, likes, id } = post;
+    const { content, username, title, likes, id, image } = post;
+    console.log(image);
     return (
         <div className={classNames(s.post)}>
+            {image !== null && <img src={image} alt='image' />}
             <div className={'flex justify-between'}>
                 <p>{username}</p>
                 <Link to={`${PathsToNavigate.POST}/${id}`}>
@@ -23,7 +26,7 @@ const Post: FC<ITestPostProps> = ({ post }) => {
                 </Link>
             </div>
             <div className={'flex justify-between'}>
-                <p>{content}</p>
+                <Content value={content} />
                 <p>{likes}</p>
             </div>
         </div>

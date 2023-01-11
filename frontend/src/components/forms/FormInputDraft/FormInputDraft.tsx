3316f9139e11a006789fde5c1fc0ab81d5709930
Ivan <ivan.kolipov@gmail.com>
2023-01-11 16:50:45 +0300

    Add FormInputDraft

---
+++ frontend/src/components/forms/FormInputDraft/FormInputDraft.tsx
@@ -0,0 +1,38 @@
+// /* eslint-disable */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { EditorState } from 'draft-js';
+import React, { FC } from 'react';
+import { SetFieldValue, UseFormWatch } from 'react-hook-form';
+
+import ReactQuill from 'react-quill';
+import 'quill/dist/quill.snow.css';
+
+import { FormCreatePostInputs } from '../FormCreatePost/FormCreatePost';
+
+interface IFormInputDraftProps {
+    control: any;
+    name: string;
+    setValue: SetFieldValue<FormCreatePostInputs>;
+    watch: UseFormWatch<FormCreatePostInputs>;
+
+    [x: string]: any;
+}
+
+const FormInputDraft: FC<IFormInputDraftProps> = ({ control, name, watch, setValue, ...props }) => {
+    const onEditorStateChange = (editorState: EditorState): void => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        setValue(name, editorState);
+    };
+
+    const editorContent = watch('content');
+
+    return (
+        <>
+            <ReactQuill theme='snow' value={editorContent} onChange={onEditorStateChange} />
+        </>
+    );
+};
+
+export default FormInputDraft;

4f32250fe895061db11e445232c3ff629177d7a4
Ivan <ivan.kolipov@gmail.com>
2023-01-11 18:52:58 +0300

    Add FetchBaseQueryError
    component with view messages on statuses

---
+++ frontend/src/components/common/FetchBaseQueryError/FetchBaseQueryError.tsx
@@ -0,0 +1,36 @@
+import { SerializedError } from '@reduxjs/toolkit';
+import { FetchBaseQueryError as TypeFetchBaseQueryError } from '@reduxjs/toolkit/query';
+import React, { FC } from 'react';
+
+import { IMessageToStatusCode } from '../../../types/IMessageToStatusCode';
+import { isFetchBaseQueryError } from '../../../utils/isFetchBaseQueryError';
+import Error from '../../forms/Error/Error';
+
+interface IFetchBaseQueryErrorProps {
+    responseError: (TypeFetchBaseQueryError | SerializedError | undefined) | null;
+    messages: IMessageToStatusCode[];
+    isError?: boolean;
+}
+
+const FetchBaseQueryError: FC<IFetchBaseQueryErrorProps> = ({ responseError, messages, isError }) => {
+    if (responseError === null || isError === false) {
+        return <></>;
+    }
+
+    if (isFetchBaseQueryError(responseError)) {
+        let message = '';
+        messages.forEach((messageToStatus) => {
+            if (messageToStatus.statusCode === responseError.status) {
+                message = messageToStatus.message;
+            }
+        });
+
+        if (message !== '') {
+            return <Error>{message}</Error>;
+        }
+    }
+
+    return <Error>Что то пошло не так</Error>;
+};
+
+export default FetchBaseQueryError;

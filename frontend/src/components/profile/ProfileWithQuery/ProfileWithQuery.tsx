9d1d3ed3dfee8aa9c33992ef286abfadee7921e7
Ivan <ivan.kolipov@gmail.com>
2023-01-11 18:56:35 +0300

    Add ServerResponse to ProfileWithQuery

--- frontend/src/components/profile/ProfileWithQuery/ProfileWithQuery.tsx
+++ frontend/src/components/profile/ProfileWithQuery/ProfileWithQuery.tsx
@@ -1,9 +1,8 @@
 import React, { FC, useState } from 'react';
 
 import { useGetUserQuery } from '../../../service/userApiSlice';
-import Loader from '../../common/Loader/Loader';
 import Pagination from '../../common/Pagination/Pagination';
-import Error from '../../forms/Error/Error';
+import ServerResponse from '../../common/ServerResponse/ServerResponse';
 import Profile from '../Profile/Profile';
 
 interface IProfileWithQueryProps {
@@ -12,7 +11,7 @@ interface IProfileWithQueryProps {
 
 const ProfileWithQuery: FC<IProfileWithQueryProps> = ({ id }) => {
     const [page, setPage] = useState<number>(1);
-    const { data, isSuccess, isLoading, isError } = useGetUserQuery({ userId: id, postsPage: page });
+    const { data, isSuccess, isLoading, isError, error } = useGetUserQuery({ userId: id, postsPage: page });
 
     const handlePageChange = ({ selected }: { selected: number }): void => {
         setPage(selected + 1);
@@ -20,14 +19,20 @@ const ProfileWithQuery: FC<IProfileWithQueryProps> = ({ id }) => {
 
     return (
         <div>
-            {isSuccess && (
-                <>
-                    <Profile profile={data} />
-                    <Pagination countPages={Math.ceil(data.count / 10)} handlePageChange={handlePageChange} />
-                </>
-            )}
-            {isLoading && <Loader />}
-            {isError && <Error>Something went wrong</Error>}
+            <ServerResponse
+                responseError={error}
+                isError={isError}
+                isLoading={isLoading}
+                isSuccess={isSuccess}
+                messages={[{ statusCode: 404, message: 'Профиль не найден' }]}
+            >
+                {data != null && (
+                    <>
+                        <Profile profile={data} />
+                        <Pagination countPages={Math.ceil(data.count / 10)} handlePageChange={handlePageChange} />
+                    </>
+                )}
+            </ServerResponse>
         </div>
     );
 };

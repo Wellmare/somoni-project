d4529ab6b9b5f7c6ce473bf9e377033a589643ea
Ivan <ivan.kolipov@gmail.com>
2023-01-12 17:24:50 +0300

    Add Tag

---
+++ frontend/src/components/Posts/Tag/Tag.tsx
@@ -0,0 +1,24 @@
+import classNames from 'classnames';
+import React, { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { PathsToNavigate } from '../../../types/Paths';
+
+interface ITagProps {
+    tag: string;
+}
+
+const Tag: FC<ITagProps> = ({ tag }) => {
+    const navigate = useNavigate();
+    const onClickTag = (): void => {
+        navigate(`${PathsToNavigate.TAG}/${tag}`);
+    };
+
+    return (
+        <div className={classNames('p-5', 'cursor-pointer')} onClick={onClickTag}>
+            {tag}
+        </div>
+    );
+};
+
+export default Tag;

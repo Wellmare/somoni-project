1bd7a010938f96c4f99d9001febc0e1ae35ec372
Ivan <ivan.kolipov@gmail.com>
2023-01-12 17:31:20 +0300

    Add tag prop to Posts

--- frontend/src/components/Posts/Posts/Posts.tsx
+++ frontend/src/components/Posts/Posts/Posts.tsx
@@ -2,12 +2,16 @@ import React, { FC, useState } from 'react';
 
 import PostsByPage from '../PostsByPage/PostsByPage';
 
-const Posts: FC = () => {
+interface IPostsProps {
+    tag?: string;
+}
+
+const Posts: FC<IPostsProps> = ({ tag }) => {
     const [page, setPage] = useState<number>(1);
 
     return (
         <>
-            <PostsByPage page={page} setPage={setPage} />
+            <PostsByPage page={page} setPage={setPage} tag={tag} />
         </>
     );
 };

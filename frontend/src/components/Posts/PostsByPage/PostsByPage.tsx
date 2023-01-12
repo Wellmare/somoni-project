08d5f7f20c324d6caf694b00cac7a02084b1e8c1
Ivan <ivan.kolipov@gmail.com>
2023-01-12 17:32:48 +0300

    Add 404 message

--- frontend/src/components/Posts/PostsByPage/PostsByPage.tsx
+++ frontend/src/components/Posts/PostsByPage/PostsByPage.tsx
@@ -8,10 +8,11 @@ import Post from '../Post/Post';
 interface IPostsByPageProps {
     page: number;
     setPage: (page: number) => void;
+    tag?: string;
 }
 
-const PostsByPage: FC<IPostsByPageProps> = ({ page, setPage }) => {
-    const { data: posts, isError, isLoading, isSuccess, error } = useGetPostsQuery({ page });
+const PostsByPage: FC<IPostsByPageProps> = ({ page, setPage, tag }) => {
+    const { data: posts, isError, isLoading, isSuccess, error } = useGetPostsQuery({ page, tag });
 
     const countPages = posts?.count != null ? Math.ceil(posts.count / 10) : 0;
 
@@ -22,7 +23,13 @@ const PostsByPage: FC<IPostsByPageProps> = ({ page, setPage }) => {
 
     return (
         <>
-            <ServerResponse responseError={error} isLoading={isLoading} isSuccess={isSuccess} isError={isError}>
+            <ServerResponse
+                responseError={error}
+                isLoading={isLoading}
+                isSuccess={isSuccess}
+                isError={isError}
+                messages={[{ statusCode: 404, message: 'Посты не найдены' }]}
+            >
                 <Pagination countPages={countPages} handlePageChange={handlePageChange} />
                 {posts?.results?.map((post) => (
                     <Post post={post} key={post.id} />

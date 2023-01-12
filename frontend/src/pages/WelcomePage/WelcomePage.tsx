68ab25b10369e25e5c0030c903b945a1f2686c8b
Ivan <ivan.kolipov@gmail.com>
2023-01-12 16:30:11 +0300

    Delete welcome component

--- frontend/src/pages/WelcomePage/WelcomePage.tsx
+++ frontend/src/pages/WelcomePage/WelcomePage.tsx
@@ -1,7 +1,20 @@
+import classNames from 'classnames';
 import React, { FC } from 'react';
 
 const WelcomePage: FC = () => {
-    return <>Welcome!</>;
+    return (
+        <section className={classNames()}>
+            <p>Пропустить</p>
+            <div className={classNames('flex', 'justify-center')}>
+                <div className={classNames('content')}>
+                    <h1>Привет!</h1>
+                    <h3>Читай там, где удобно!</h3>
+                    {/* <button>Войти</button> */}
+                    {/* <button>Регистрация</button> */}
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default WelcomePage;

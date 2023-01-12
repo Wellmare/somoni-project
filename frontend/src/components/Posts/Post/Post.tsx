de019224f8a90f255f519aedfe3a541f276c05fd
Ivan <ivan.kolipov@gmail.com>
2023-01-12 17:31:05 +0300

    Add username link and tags to Post

--- frontend/src/components/Posts/Post/Post.tsx
+++ frontend/src/components/Posts/Post/Post.tsx
@@ -8,18 +8,21 @@ import s from './Post.module.scss';
 import { PathsToNavigate } from '../../../types/Paths';
 import { IPost } from '../../../types/redux/posts/IPost';
 import Content from '../Content/Content';
+import Tag from '../Tag/Tag';
 
 interface ITestPostProps {
     post: IPost;
 }
 
 const Post: FC<ITestPostProps> = ({ post }) => {
-    const { content, username, title, likes, id, image } = post;
+    const { content, username, title, likes, id, image, author: authorId, tags } = post;
     return (
         <div className={classNames(s.post)}>
             {image !== null && <img src={image} alt='image' className={classNames(s.image)} />}
             <div className={'flex justify-between'}>
-                <p>{username}</p>
+                <Link to={`${PathsToNavigate.USER}/${authorId}`}>
+                    <p>{username}</p>
+                </Link>
                 <Link to={`${PathsToNavigate.POST}/${id}`}>
                     <p>{title}</p>
                 </Link>
@@ -27,6 +30,9 @@ const Post: FC<ITestPostProps> = ({ post }) => {
             <div className={'flex justify-between'}>
                 <Content value={content} />
                 <p>{likes}</p>
+                <div className={classNames('mt-auto', 'flex', 'justify-around')}>
+                    {tags?.length > 0 && tags.map((tag) => <Tag tag={tag} key={tag} />)}
+                </div>
             </div>
         </div>
     );

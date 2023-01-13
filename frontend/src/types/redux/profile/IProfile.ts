8df0520ebabcf207f174c4cc726bf208ab0a49d0
Ivan <ivan.kolipov@gmail.com>
2023-01-13 19:39:02 +0300

    Add email and isMyProfile to IProfile

--- frontend/src/types/redux/profile/IProfile.ts
+++ frontend/src/types/redux/profile/IProfile.ts
@@ -2,11 +2,15 @@ import { Link } from '../Link';
 import { IPost } from '../posts/IPost';
 
 export interface IProfile {
+    next: Link;
+    previous: Link;
+
     count: number;
     username: string;
     photo: string | null;
     bio: string | null;
+    email?: string;
+    isMyProfile: boolean;
+
     results: IPost[] | null;
-    next: Link;
-    previous: Link;
 }

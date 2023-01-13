a367d9a6cd61c3b2480f9f11b9550550cbbd89c8
Ivan <ivan.kolipov@gmail.com>
2023-01-13 16:55:41 +0300

    Enhance PostSkeleton

--- frontend/src/components/skeletons/PostSkeleton.tsx
+++ frontend/src/components/skeletons/PostSkeleton.tsx
@@ -5,30 +5,31 @@ const PostSkeleton: FC = () => {
     return (
         <ContentLoader
             speed={2}
-            width={400}
-            height={460}
-            viewBox='0 0 400 460'
+            width={600}
+            height={350}
+            viewBox='0 0 600 350'
             backgroundColor='#f3f3f3'
             foregroundColor='#ecebeb'
+            className={'w-full'}
         >
-            <circle cx='28' cy='45' r='25' />
-            <rect x='3' y='83' rx='17' ry='17' width='390' height='110' />
-            <rect x='3' y='210' rx='6' ry='6' width='200' height='12' />
-            <rect x='217' y='210' rx='6' ry='6' width='50' height='12' />
-            <rect x='281' y='210' rx='6' ry='6' width='110' height='12' />
-            <rect x='61' y='233' rx='6' ry='6' width='200' height='12' />
-            <rect x='3' y='233' rx='6' ry='6' width='50' height='12' />
-            <rect x='276' y='234' rx='6' ry='6' width='110' height='12' />
-            <rect x='3' y='255' rx='6' ry='6' width='200' height='12' />
-            <rect x='217' y='255' rx='6' ry='6' width='50' height='12' />
-            <rect x='281' y='255' rx='6' ry='6' width='110' height='12' />
-            <rect x='61' y='278' rx='6' ry='6' width='200' height='12' />
-            <rect x='3' y='278' rx='6' ry='6' width='50' height='12' />
-            <rect x='276' y='279' rx='6' ry='6' width='110' height='12' />
-            <rect x='4' y='299' rx='6' ry='6' width='200' height='12' />
-            <rect x='231' y='299' rx='6' ry='6' width='110' height='12' />
-            <rect x='65' y='27' rx='6' ry='6' width='150' height='12' />
-            <rect x='67' y='51' rx='6' ry='6' width='100' height='12' />
+            <circle cx='28' cy='43' r='25' />
+            <rect x='2' y='81' rx='17' ry='17' width='587' height='145' />
+            <rect x='2' y='244' rx='6' ry='6' width='301' height='12' />
+            <rect x='314' y='244' rx='6' ry='6' width='90' height='12' />
+            <rect x='419' y='244' rx='6' ry='6' width='165' height='12' />
+            <rect x='88' y='267' rx='6' ry='6' width='301' height='12' />
+            <rect x='2' y='267' rx='6' ry='6' width='76' height='12' />
+            <rect x='400' y='268' rx='6' ry='6' width='165' height='12' />
+            <rect x='2' y='290' rx='6' ry='6' width='301' height='12' />
+            <rect x='318' y='290' rx='6' ry='6' width='76' height='12' />
+            <rect x='409' y='290' rx='6' ry='6' width='165' height='12' />
+            <rect x='88' y='313' rx='6' ry='6' width='301' height='12' />
+            <rect x='2' y='313' rx='6' ry='6' width='76' height='12' />
+            <rect x='400' y='314' rx='6' ry='6' width='165' height='12' />
+            <rect x='3' y='334' rx='6' ry='6' width='301' height='12' />
+            <rect x='319' y='335' rx='6' ry='6' width='165' height='12' />
+            <rect x='66' y='25' rx='6' ry='6' width='200' height='12' />
+            <rect x='68' y='49' rx='6' ry='6' width='101' height='12' />
         </ContentLoader>
     );
 };

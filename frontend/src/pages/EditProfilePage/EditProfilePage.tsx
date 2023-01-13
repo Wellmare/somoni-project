d0e1ccc9427d0d3872ca40490880f3c84d4e0272
Ivan <ivan.kolipov@gmail.com>
2023-01-13 19:53:35 +0300

    Add useGetDataToEditUserQuery data to edit profile

--- frontend/src/pages/EditProfilePage/EditProfilePage.tsx
+++ frontend/src/pages/EditProfilePage/EditProfilePage.tsx
@@ -1,34 +1,23 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 
 import ServerResponse from '../../components/common/ServerResponse/ServerResponse';
 import EditProfileForm from '../../components/forms/EditProfileForm/EditProfileForm';
-import { useAppSelector } from '../../hooks/reduxHooks';
-import { useLazyGetUserQuery } from '../../service/userApiSlice';
-import { doAsyncFunc } from '../../utils/doAsyncFunc';
+import { useGetDataToEditUserQuery } from '../../service/userApiSlice';
 
 const EditProfilePage: FC = () => {
-    const userId = useAppSelector((state) => state?.auth?.user?.user_id);
-    const user = useAppSelector((state) => state?.auth?.user);
-    const [getUser, { error, isError, isLoading, data, isSuccess }] = useLazyGetUserQuery();
-
-    useEffect(() => {
-        if (userId !== undefined) {
-            doAsyncFunc(async () => {
-                await getUser({ userId: userId.toString() });
-            });
-        }
-    }, []);
+    const { data, error, isError, isLoading, isSuccess } = useGetDataToEditUserQuery(undefined);
 
     return (
         <>
             <ServerResponse responseError={error} isError={isError} isLoading={isLoading} isSuccess={isSuccess}>
-                {data !== undefined && user !== null && (
+                {data !== undefined && (
                     <EditProfileForm
                         defaultValues={{
                             username: data.username,
                             bio: data.bio === null ? undefined : data.bio,
-                            email: user.email,
+                            email: data.email,
                         }}
+                        photo={data.photo}
                     />
                 )}
             </ServerResponse>

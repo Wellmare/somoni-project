e49b0fe67cf5f0075c4bf175d310d0cbe015af64
Ivan <ivan.kolipov@gmail.com>
2023-01-14 19:36:33 +0300

    Add view FormCreateComment when isAuth

--- frontend/src/components/Comments/Comments/Comments.tsx
+++ frontend/src/components/Comments/Comments/Comments.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 
+import { useAppSelector } from '../../../hooks/reduxHooks';
+import { selectIsAuth } from '../../../redux/slices/authSlice';
 import { useGetCommentsQuery } from '../../../service/commentsApiSlice';
 import ServerResponse from '../../common/ServerResponse/ServerResponse';
 import FormCreateComment from '../../forms/FormCreateComment/FormCreateComment';
@@ -11,11 +13,12 @@ interface ICommentsProps {
 
 const Comments: FC<ICommentsProps> = ({ postId }) => {
     const { isSuccess, isLoading, data, error, isError } = useGetCommentsQuery({ postId });
+    const isAuth = useAppSelector(selectIsAuth);
 
     return (
         <>
             <ServerResponse responseError={error} isError={isError} isLoading={isLoading} isSuccess={isSuccess}>
-                <FormCreateComment postId={postId} />
+                {isAuth && <FormCreateComment postId={postId} />}
                 {data?.results?.map((comment) => (
                     <Comment comment={comment} key={comment.id} />
                 ))}

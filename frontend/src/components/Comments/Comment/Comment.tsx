d72ab08040d3baef060bddf7b6310866ac445ea7
Ivan <ivan.kolipov@gmail.com>
2023-01-14 17:09:23 +0300

    Add isMyProfile and delete comment

--- frontend/src/components/Comments/Comment/Comment.tsx
+++ frontend/src/components/Comments/Comment/Comment.tsx
@@ -1,18 +1,35 @@
 import React, { FC } from 'react';
 
+import { useDeleteCommentMutation } from '../../../service/commentsApiSlice';
 import { IComment } from '../../../types/redux/comments/IComment';
+import { doAsyncFunc } from '../../../utils/doAsyncFunc';
 
 interface ICommentProps {
     comment: IComment;
 }
 
 const Comment: FC<ICommentProps> = ({ comment }) => {
-    const { content, author, id, date, photo: avatar, username } = comment;
+    const { content, author, id, date, photo: avatar, username, isMyComment } = comment;
+    const [deleteComment] = useDeleteCommentMutation();
+
+    console.log(isMyComment);
+
+    const onDelete = (): void => {
+        doAsyncFunc(async () => {
+            await deleteComment({ commentId: id.toString() });
+        });
+    };
+
     return (
         <>
             {avatar != null && <img src={avatar} alt='avatar' />}
             <p>{username}</p>
             <p>{content}</p>
+            {isMyComment && (
+                <div className={'text-red-600'} onClick={onDelete}>
+                    Delete
+                </div>
+            )}
         </>
     );
 };

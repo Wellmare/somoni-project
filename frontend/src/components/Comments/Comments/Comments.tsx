b8e5c4bc4a9ffb176f9cdb9afa914525b234fbc4
Ivan <ivan.kolipov@gmail.com>
2023-01-14 17:09:58 +0300

    Add FormCreateComment to Comments

--- frontend/src/components/Comments/Comments/Comments.tsx
+++ frontend/src/components/Comments/Comments/Comments.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 
 import { useGetCommentsQuery } from '../../../service/commentsApiSlice';
 import ServerResponse from '../../common/ServerResponse/ServerResponse';
+import FormCreateComment from '../../forms/FormCreateComment/FormCreateComment';
 import Comment from '../Comment/Comment';
 
 interface ICommentsProps {
@@ -10,9 +11,11 @@ interface ICommentsProps {
 
 const Comments: FC<ICommentsProps> = ({ postId }) => {
     const { isSuccess, isLoading, data, error, isError } = useGetCommentsQuery({ postId });
+
     return (
         <>
             <ServerResponse responseError={error} isError={isError} isLoading={isLoading} isSuccess={isSuccess}>
+                <FormCreateComment postId={postId} />
                 {data?.results?.map((comment) => (
                     <Comment comment={comment} key={comment.id} />
                 ))}

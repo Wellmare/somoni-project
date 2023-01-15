d25fef01a7977d971606652eef4204489c25bb67
Ivan <ivan.kolipov@gmail.com>
2023-01-15 18:29:27 +0300

    Add edit post path

--- frontend/src/utils/pathsToNavigate.ts
+++ frontend/src/utils/pathsToNavigate.ts
@@ -4,4 +4,5 @@ export const pathsToNavigate = {
     user: (userId: string) => `${PathsToNavigate.USER}/${userId}`,
     post: (postId: string) => `${PathsToNavigate.POST}/${postId}`,
     tag: (tag: string) => `${PathsToNavigate.TAG}/${tag}`,
+    editPost: (postId: string) => `${PathsToNavigate.EDIT_POST}/${postId}`,
 };

43c91f9e7f592eba5599da284a514c55a248de3f
Ivan <ivan.kolipov@gmail.com>
2023-01-15 17:48:03 +0300

    FIX user id }

--- frontend/src/utils/pathsToNavigate.ts
+++ frontend/src/utils/pathsToNavigate.ts
@@ -1,7 +1,7 @@
 import { PathsToNavigate } from '../constants/Paths';
 
 export const pathsToNavigate = {
-    user: (userId: string) => `${PathsToNavigate.USER}/${userId}}`,
+    user: (userId: string) => `${PathsToNavigate.USER}/${userId}`,
     post: (postId: string) => `${PathsToNavigate.POST}/${postId}`,
     tag: (tag: string) => `${PathsToNavigate.TAG}/${tag}`,
 };

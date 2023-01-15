e38578e3f8d69411c05c8d8ff38991d2f6a567bf
Danila <morozovd0605@gmail.com>
2023-01-15 14:55:14 +0300

    fix url

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -8,10 +8,10 @@ from blog import views
 
 urlpatterns = [
     path('posts/', views.get_create_post.as_view()),
-    path('posts/<slug:tag_slug>', views.get_create_post.as_view()),
-    path('post/<int:pk>', views.post_detail_view.as_view()),
-    path('user/<int:pk>', views.get_post_for_user.as_view()),
-    path('post/<int:pk>/like', views.like_post),
-    path('post/<int:pk>/comments', views.get_create_comments.as_view()),
-    path('comment/<int:pk>', views.comment_detail_view.as_view())
+    path('posts/<slug:tag_slug>/', views.get_create_post.as_view()),
+    path('post/<int:pk>/', views.post_detail_view.as_view()),
+    path('user/<int:pk>/', views.get_post_for_user.as_view()),
+    path('post/<int:pk>/like/', views.like_post),
+    path('post/<int:pk>/comments/', views.get_create_comments.as_view()),
+    path('comment/<int:pk>/', views.comment_detail_view.as_view())
 ]

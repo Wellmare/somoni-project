759d146774f59585a5cdf7bfff406ea129145918
Ivan <ivan.kolipov@gmail.com>
2023-01-17 17:55:17 +0300

    Add link to post

--- frontend/src/components/posts/post/PostTitle/PostTitle.tsx
+++ frontend/src/components/posts/post/PostTitle/PostTitle.tsx
@@ -1,11 +1,19 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+import { pathsToNavigate } from '../../../../utils/pathsToNavigate';
 
 interface IPostTitleProps {
     title: string;
+    postId: string;
 }
 
-const PostTitle: FC<IPostTitleProps> = ({ title }) => {
-    return <h3>{title}</h3>;
+const PostTitle: FC<IPostTitleProps> = ({ title, postId }) => {
+    return (
+        <Link to={pathsToNavigate.post(postId)}>
+            <h3>{title}</h3>
+        </Link>
+    );
 };
 
 export default PostTitle;

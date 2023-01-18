5008de6eafd570c6ed2a864abf0f4420461c04e9
Ivan <ivan.kolipov@gmail.com>
2023-01-18 17:18:32 +0300

    Add confirm delete post

--- frontend/src/components/posts/post/PostDeleteButton/PostDeleteButton.tsx
+++ frontend/src/components/posts/post/PostDeleteButton/PostDeleteButton.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
@@ -6,6 +6,7 @@ import deleteIcon from '../../../../assets/delete.svg';
 import { PathsToNavigate } from '../../../../constants/Paths';
 import { useDeletePostMutation } from '../../../../service/postsApiSlice';
 import { doAsyncFunc } from '../../../../utils/doAsyncFunc';
+import Alert from '../../../common/Alert/Alert';
 import s from '../PostButton/PostButton.module.scss';
 
 interface IPostDeleteButtonProps {
@@ -13,9 +14,14 @@ interface IPostDeleteButtonProps {
 }
 
 const PostDeleteButton: FC<IPostDeleteButtonProps> = ({ postId }) => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
     const [deletePost] = useDeletePostMutation();
     const navigate = useNavigate();
-    const onClick = (): void => {
+    const openDeleteAlert = (): void => {
+        setIsOpen(true);
+    };
+    const onDeletePost = (): void => {
         doAsyncFunc(async () => {
             try {
                 await deletePost({ id: postId }).unwrap();
@@ -27,9 +33,24 @@ const PostDeleteButton: FC<IPostDeleteButtonProps> = ({ postId }) => {
     };
 
     return (
-        <div className={s.button} onClick={onClick}>
-            <img src={deleteIcon} alt='delete' />
-        </div>
+        <>
+            <div className={s.button} onClick={openDeleteAlert}>
+                <img src={deleteIcon} alt='delete' />
+            </div>
+            <Alert
+                title={'Удалить пост?'}
+                text={'Отменить это действие будет невозможно'}
+                isOpen={isOpen}
+                onSuccess={() => {
+                    onDeletePost();
+                    setIsOpen(false);
+                }}
+                onClose={() => {
+                    setIsOpen(false);
+                }}
+                buttonText={'Удалить пост'}
+            />
+        </>
     );
 };
 

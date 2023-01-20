52775da55d52c655257af23aa498d7acf9561221
Ivan <ivan.kolipov@gmail.com>
2023-01-20 16:16:06 +0300

    Add IEdit

--- frontend/src/context/CommentContext.ts
+++ frontend/src/context/CommentContext.ts
@@ -1,13 +1,11 @@
-import React, { createContext } from 'react';
+import { createContext } from 'react';
 
+import { IEdit } from '../types/IEdit';
 import { IComment } from '../types/redux/comments/IComment';
 
 interface ICommentContext {
     comment: IComment | null;
-    edit: null | {
-        isEdit: boolean;
-        setIsEdit: React.Dispatch<React.SetStateAction<boolean>>;
-    };
+    edit: null | IEdit;
 }
 
 export const CommentContext = createContext<ICommentContext>({ comment: null, edit: null });

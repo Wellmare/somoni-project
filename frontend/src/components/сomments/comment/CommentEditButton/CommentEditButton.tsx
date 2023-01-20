b1f20783804e98700f354d848bd48fe7d70d1021
Ivan <ivan.kolipov@gmail.com>
2023-01-20 08:24:56 +0300

    Add edit icon to CommentEditButton

--- frontend/src/components/сomments/comment/CommentEditButton/CommentEditButton.tsx
+++ frontend/src/components/сomments/comment/CommentEditButton/CommentEditButton.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames';
 import React, { FC } from 'react';
 
+import EditIcon from '../../../../assets/edit.svg';
+
 interface ICommentEditButtonProps {
     setIsEdit: React.Dispatch<React.SetStateAction<boolean>>;
 }
@@ -8,7 +10,9 @@ interface ICommentEditButtonProps {
 const CommentEditButton: FC<ICommentEditButtonProps> = ({ setIsEdit }) => {
     return (
         <div className={classNames('cursor-pointer', 'ml-5')} onClick={() => setIsEdit((old) => !old)}>
-            Edit
+            <div className={'icon'}>
+                <img src={EditIcon} alt='edit' />
+            </div>
         </div>
     );
 };

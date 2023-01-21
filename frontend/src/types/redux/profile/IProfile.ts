930169b7bde1b379e2f4a836df4860f380fd7b24
Ivan <ivan.kolipov@gmail.com>
2023-01-21 10:57:33 +0300

    Add IProfile

--- frontend/src/types/redux/profile/IProfile.ts
+++ frontend/src/types/redux/profile/IProfile.ts
@@ -1,17 +1,8 @@
-import { LinkType } from '../LinkType';
-import { IPostServerResponse } from '../posts/IPostServerResponse';
-
 export interface IProfile {
-    next: LinkType;
-    previous: LinkType;
-
-    count: number;
     username: string;
-    photo: string;
+    avatarLink: string;
     bio: string | null;
     email?: string;
     isMyProfile: boolean;
-    id: string;
-
-    results: IPostServerResponse[] | null;
+    profileId: string;
 }

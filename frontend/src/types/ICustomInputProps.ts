1dea2abb86297767314dbaa330a78bb2fc6fae45
Ivan <ivan.kolipov@gmail.com>
2023-01-21 18:00:05 +0300

    Add name to ICustomInputProps

--- frontend/src/types/ICustomInputProps.ts
+++ frontend/src/types/ICustomInputProps.ts
@@ -4,4 +4,5 @@ import { IInputProps } from '../components/common/FormInput/FormInput';
 
 export interface ICustomInputProps extends Partial<IInputProps> {
     control: Control<any>;
+    name?: string;
 }

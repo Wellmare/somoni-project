cfed66c7edd7e9659678abe9c0165669ac6bc041
Ivan <ivan.kolipov@gmail.com>
2023-01-21 10:55:30 +0300

    Add enhanceIPostServerResponse.ts

---
+++ frontend/src/utils/enhanceIPostServerResponse.ts
@@ -0,0 +1,36 @@
+import { IPost } from '../types/redux/posts/IPost';
+import { IPostServerResponse } from '../types/redux/posts/IPostServerResponse';
+
+export const enhanceIPostServerResponse = (postServerResponse: IPostServerResponse): IPost => {
+    const {
+        content,
+        username: authorUsername,
+        title,
+        likes: likesCount,
+        id: postId,
+        image: postImageLink,
+        author: authorId,
+        tags,
+        isLiked,
+        isMyPost,
+        photo: avatarLink,
+        date: dateString,
+        comments: commentsCount,
+    } = postServerResponse;
+
+    return {
+        tags,
+        isMyPost,
+        postId: postId.toString(),
+        title,
+        content,
+        userId: authorId.toString(),
+        username: authorUsername,
+        isLiked,
+        avatarLink,
+        dateString,
+        postImageLink,
+        likesCount,
+        commentsCount,
+    };
+};

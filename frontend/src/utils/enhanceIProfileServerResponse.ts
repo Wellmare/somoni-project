af658a6c0b79ec14c9c4ae719c351fdec5bd027c
Ivan <ivan.kolipov@gmail.com>
2023-01-21 10:55:42 +0300

    Add enhanceIProfileServerResponse.ts

---
+++ frontend/src/utils/enhanceIProfileServerResponse.ts
@@ -0,0 +1,29 @@
+import { enhanceIPostServerResponse } from './enhanceIPostServerResponse';
+
+import { IPost } from '../types/redux/posts/IPost';
+import { IPaginatedProfileResponse } from '../types/redux/profile/IPaginatedProfileResponse';
+import { IProfile } from '../types/redux/profile/IProfile';
+
+export const enhanceIProfileServerResponse = (
+    profileServerResponse: IPaginatedProfileResponse,
+): { profile: IProfile; posts: IPost[] | null } => {
+    const { results, isMyProfile, bio, id, email, username, photo } = profileServerResponse;
+
+    const enhancedProfile: IProfile = {
+        isMyProfile,
+        profileId: id,
+        email,
+        username,
+        bio,
+        avatarLink: photo,
+    };
+    if (results === null) {
+        return { posts: null, profile: enhancedProfile };
+    }
+
+    const posts = results.map((post) => {
+        return enhanceIPostServerResponse(post);
+    });
+
+    return { posts, profile: enhancedProfile };
+};

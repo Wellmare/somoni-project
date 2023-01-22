3b1ea89e56d910dca8224f13b460d6e13a37df0c
Ivan <ivan.kolipov@gmail.com>
2023-01-22 17:10:13 +0300

    Add '0' to minutes

--- frontend/src/utils/getLocalDateFromString.ts
+++ frontend/src/utils/getLocalDateFromString.ts
@@ -2,7 +2,10 @@ export const getLocalDateFromString = (date: string): string => {
     const parsedDate = new Date(date);
 
     const localeDate = parsedDate.toLocaleDateString();
-    const time = `${parsedDate.getHours()}:${parsedDate.getMinutes()}`;
+    const hour = parsedDate.getHours();
+    const minute = parsedDate.getMinutes() < 10 ? `0${parsedDate.getMinutes()}` : parsedDate.getMinutes();
+
+    const time = `${hour}:${minute}`;
 
     return `${localeDate}: ${time}`;
 };

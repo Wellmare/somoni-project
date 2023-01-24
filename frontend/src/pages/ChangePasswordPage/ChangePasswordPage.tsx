3dd518b12d661b6bf6df92ceb4b3a4084eb54975
Ivan <ivan.kolipov@gmail.com>
2023-01-24 15:10:37 +0300

    Add WIP changePasswordPage

--- frontend/src/pages/ChangePasswordPage/ChangePasswordPage.tsx
+++ frontend/src/pages/ChangePasswordPage/ChangePasswordPage.tsx
@@ -1,7 +1,35 @@
 import React, { FC } from 'react';
 
+import ServerResponse from '../../components/server/ServerResponse/ServerResponse';
+import { useAppSelector } from '../../hooks/reduxHooks';
+import { selectTokens } from '../../redux/slices/authSlice';
+import { useChangePasswordMutation } from '../../service/authApiSlice';
+import { doAsyncFunc } from '../../utils/doAsyncFunc';
+
 const ChangePasswordPage: FC = () => {
-    return <div></div>;
+    const [changePassword, { isSuccess, error, isError, isLoading }] = useChangePasswordMutation();
+    const refresh = useAppSelector(selectTokens)?.refresh;
+    const onChange = (): void => {
+        if (refresh == null) return;
+
+        doAsyncFunc(async () => {
+            await changePassword({
+                password: 'tear12?SOMONI',
+                password2: 'tear12?SOMONI',
+                oldPassword: 'tear12?SOM',
+                refresh,
+            });
+        });
+    };
+
+    return (
+        <div>
+            <button onClick={onChange}>change</button>
+            <ServerResponse responseError={error} isError={isError} isLoading={isLoading} isSuccess={isSuccess}>
+                password changed
+            </ServerResponse>
+        </div>
+    );
 };
 
 export default ChangePasswordPage;

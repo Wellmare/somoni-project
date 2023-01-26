7b3b9ad0c11fa440f580ce24e2f87e8b99b8a08d
Ivan <ivan.kolipov@gmail.com>
2023-01-26 17:40:03 +0300

    Add HeaderMenu

--- frontend/src/components/header/HeaderMenu/HeaderMenu.tsx
+++ frontend/src/components/header/HeaderMenu/HeaderMenu.tsx
@@ -1,10 +1,9 @@
-import { Menu as ReactMenu, MenuItem as MenuItemInner, MenuItemProps } from '@szhsin/react-menu';
 import classNames from 'classnames';
 import React, { FC, useContext } from 'react';
 
 import { Link } from 'react-router-dom';
 
-import s from './Menu.module.scss';
+import s from './HeaderMenu.module.scss';
 
 import '@szhsin/react-menu/dist/transitions/slide.css';
 import { ReactComponent as ArrowDownIcon } from '../../../assets/svg/arrow-down.svg';
@@ -15,21 +14,14 @@ import { AvatarSize } from '../../../types/UI/Avatar.types';
 import Avatar from '../../../ui/Avatar/Avatar';
 import Icon from '../../../ui/Icon/Icon';
 import { pathsToNavigate } from '../../../utils/pathsToNavigate';
-
-const menuClassName = ({ state }: { state: string }): string =>
-    state === 'opening' ? s.menuOpening : state === 'opening' ? s.menuClosing : s.menu;
-
-const menuItemClassName = ({ hover, disabled }: { hover: boolean; disabled: boolean }): string =>
-    disabled ? s.menuItemDisabled : hover ? s.menuItemHover : s.menuItem;
-
-const MenuItem = (props?: MenuItemProps): JSX.Element => <MenuItemInner {...props} className={menuItemClassName} />;
+import Menu, { MenuItem } from '../../Menu/Menu';
 
 interface IMenuProps {
     photo: string;
     id: string;
 }
 
-const Menu: FC<IMenuProps> = ({ photo, id }) => {
+const HeaderMenu: FC<IMenuProps> = ({ photo, id }) => {
     const themeContext = useContext(ThemeContext);
     const onChangeTheme = (): void => {
         themeContext?.setTheme(themeContext?.theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
@@ -39,7 +31,7 @@ const Menu: FC<IMenuProps> = ({ photo, id }) => {
     const pathToProfile = pathsToNavigate.user(id);
 
     return (
-        <ReactMenu
+        <Menu
             menuButton={
                 <div className={classNames('cursor-pointer', 'flex', 'items-center', s.menuButton)}>
                     <Avatar size={AvatarSize.small}>
@@ -50,8 +42,6 @@ const Menu: FC<IMenuProps> = ({ photo, id }) => {
                     </Icon>
                 </div>
             }
-            transition
-            menuClassName={menuClassName}
         >
             {/* Для пк */}
             <div className={classNames('hidden', 'sm:block')}>
@@ -75,8 +65,46 @@ const Menu: FC<IMenuProps> = ({ photo, id }) => {
                 <div className={s.menuDivider} />
                 <MenuItem onClick={() => logoutUser()}>Выйти</MenuItem>
             </div>
-        </ReactMenu>
+        </Menu>
     );
 };
 
-export default Menu;
+export default HeaderMenu;
+
+// /* // <ReactMenu
+//         //     menuButton={
+//         //         <div className={classNames('cursor-pointer', 'flex', 'items-center', s.menuButton)}>
+//         //             <Avatar size={AvatarSize.small}>
+//         //                 <img src={photo} alt='profile photo' />
+//         //             </Avatar>
+//         //             <Icon customTypeClassName={s.arrowIcon}>
+//         //                 <ArrowDownIcon />
+//         //             </Icon>
+//         //         </div>
+//         //     }
+//         //     transition
+//         //     menuClassName={menuClassName}
+//         // >
+//             {/* /!* Для пк *!/ */}
+// {/* <div className={classNames('hidden', 'sm:block')}> */}
+// {/*     <Link to={pathToProfile}> */}
+// {/*         <MenuItem>Профиль</MenuItem> */}
+// {/*     </Link> */}
+// {/*     <div className={s.menuDivider} /> */}
+// {/*     <MenuItem onClick={onChangeTheme}>Сменить тему</MenuItem> */}
+// {/*     <div className={s.menuDivider} /> */}
+// {/*     <MenuItem onClick={() => logoutUser()}>Выйти</MenuItem> */}
+// {/* </div> */}
+//
+// {/* /!* Для мобилы *!/ */}
+// {/* <div className={classNames('block', 'sm:hidden')}> */}
+// {/*     <Link to={pathToProfile}> */}
+// {/*         <MenuItem>Профиль</MenuItem> */}
+// {/*     </Link> */}
+//
+// {/*     <div className={s.menuDivider} /> */}
+// {/*     <MenuItem onClick={onChangeTheme}>Сменить тему</MenuItem> */}
+// {/*     <div className={s.menuDivider} /> */}
+// {/*     <MenuItem onClick={() => logoutUser()}>Выйти</MenuItem> */}
+// {/* </div> */}
+// // </ReactMenu> */

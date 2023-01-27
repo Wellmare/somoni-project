964f0de8796a3bbfc5d7da922c203c2e687ac1d2
Ivan <ivan.kolipov@gmail.com>
2023-01-27 20:50:07 +0300

    Remove logic

--- frontend/src/pages/ChangePasswordPage/ChangePasswordPage.tsx
+++ frontend/src/pages/ChangePasswordPage/ChangePasswordPage.tsx
@@ -1,35 +1,9 @@
 import React, { FC } from 'react';
 
-import ServerResponse from '../../components/server/ServerResponse/ServerResponse';
-import { useAppSelector } from '../../hooks/reduxHooks';
-import { selectTokens } from '../../redux/slices/authSlice';
-import { useChangePasswordMutation } from '../../service/authApiSlice';
-import { doAsyncFunc } from '../../utils/doAsyncFunc';
+import ChangePasswordForm from '../../components/forms/auth/ChangePasswordForm/ChangePasswordForm';
 
 const ChangePasswordPage: FC = () => {
-    const [changePassword, { isSuccess, error, isError, isLoading }] = useChangePasswordMutation();
-    const refresh = useAppSelector(selectTokens)?.refresh;
-    const onChange = (): void => {
-        if (refresh == null) return;
-
-        doAsyncFunc(async () => {
-            await changePassword({
-                password: 'tear12?SOMONI',
-                password2: 'tear12?SOMONI',
-                oldPassword: 'tear12?SOM',
-                refresh,
-            });
-        });
-    };
-
-    return (
-        <div>
-            <button onClick={onChange}>change</button>
-            <ServerResponse responseError={error} isError={isError} isLoading={isLoading} isSuccess={isSuccess}>
-                password changed
-            </ServerResponse>
-        </div>
-    );
+    return <ChangePasswordForm />;
 };
 
 export default ChangePasswordPage;

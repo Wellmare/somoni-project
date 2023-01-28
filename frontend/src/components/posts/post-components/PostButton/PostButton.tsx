7967b856140dbd53478be62cbd5d2fd3d7984497
Ivan <ivan.kolipov@gmail.com>
2023-01-28 11:34:38 +0300

    Add styles for PostButton

--- frontend/src/components/posts/post-components/PostButton/PostButton.tsx
+++ frontend/src/components/posts/post-components/PostButton/PostButton.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames';
 import React, { FC, ReactNode } from 'react';
 
+import s from './PostButton.module.scss';
+
 interface IPostButtonProps {
     children: ReactNode;
     count: number;
@@ -9,8 +11,11 @@ interface IPostButtonProps {
 
 const PostButton: FC<IPostButtonProps> = ({ children, onClick, count }) => {
     return (
-        <div className={classNames('flex', 'justify-between', 'items-center')} onClick={onClick}>
-            {children} <div className={'ml-3'}>{count}</div>
+        <div
+            className={classNames('flex', 'justify-between', 'items-center', 'cursor-pointer', s.postButton)}
+            onClick={onClick}
+        >
+            {children} <div className={'ml-2'}>{count}</div>
         </div>
     );
 };

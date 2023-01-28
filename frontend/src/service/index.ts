0ad9ef69f0f67f701e53b96472e741bc211988d1
Ivan <ivan.kolipov@gmail.com>
2023-01-28 17:27:02 +0300

    Change error handling to isTokenExpired

--- frontend/src/service/index.ts
+++ frontend/src/service/index.ts
@@ -6,6 +6,7 @@ import { apiEndpoints } from '../constants/apiEndpoints';
 import { logout, setAuthTokens } from '../redux/slices/authSlice';
 import { RootState } from '../redux/store';
 import { ITokens } from '../types/redux/auth/ITokens';
+import { isTokenExpired } from '../utils/isTokenExpired';
 
 const baseQuery = fetchBaseQuery({
     baseUrl: url,
@@ -22,42 +23,63 @@ const baseQueryWithReAuth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQue
     api,
     extraOptions,
 ) => {
-    let result = await baseQuery(args, api, extraOptions);
+    const result = await baseQuery(args, api, extraOptions);
+    const authTokens = (api.getState() as RootState).auth?.authTokens;
 
-    if (!(api.getState() as RootState).auth.isAuth) return result;
+    console.log('authTokens', authTokens);
 
-    if (result.error != null && result.error.status === 401) {
-        console.log('not auth');
-        // try to get a new token
-        console.log('try auth');
-        const refreshResult = await baseQuery(
-            {
-                url: apiEndpoints.refreshToken,
-                method: 'POST',
-                body: {
-                    refresh: (api.getState() as RootState).auth?.authTokens?.refresh,
-                },
+    if (authTokens === null) return result;
+
+    if (!isTokenExpired(authTokens.access)) {
+        // noinspection UnnecessaryLocalVariableJS
+        const result = await baseQuery(args, api, extraOptions);
+        return result;
+    }
+
+    // const isAuth = (api.getState() as RootState).auth.isAuth;
+    // const authTokens = getAuthTokensFromLocalStorage();
+
+    // console.log('isAuth', isAuth);
+    // console.log('authTokens from state', authTokens);
+    //
+    // if (!isAuth) return result;
+
+    // if (result.error != null && result.error.status === 401 && authTokens !== null && authTokens.refresh !== '') {
+    //     // console.log('not auth');
+    //     // try to get a new token
+    //     console.log('try auth');
+    const refreshResult = await baseQuery(
+        {
+            url: apiEndpoints.refreshToken,
+            method: 'POST',
+            body: {
+                refresh: authTokens.refresh,
             },
-            api,
-            extraOptions,
-        );
-        if (refreshResult.data !== null) {
-            // store the new token
-            const newTokens = refreshResult.data as ITokens;
+        },
+        api,
+        extraOptions,
+    );
+    if (refreshResult.data !== null) {
+        // store the new token
+        console.log(refreshResult);
+        const newTokens = refreshResult.data as ITokens;
 
-            if (newTokens === undefined || !('refresh' in newTokens) || !('access' in newTokens)) {
-                console.log('Tokens is undefined!');
-                return result;
-            }
-
-            api.dispatch(setAuthTokens(refreshResult.data as ITokens));
-            // retry the initial query
-            result = await baseQuery(args, api, extraOptions);
-            console.log('set auth');
-        } else {
-            console.log('no data');
-            api.dispatch(logout());
+        if (newTokens === undefined || !('refresh' in newTokens) || !('access' in newTokens)) {
+            console.log('Tokens is undefined!');
+            return result;
         }
+
+        // setAuthTokensToLocalStorage(newTokens);
+
+        api.dispatch(setAuthTokens(refreshResult.data as ITokens));
+        // retry the initial query
+
+        console.log('set auth');
+        // noinspection ES6RedundantAwait
+        return await baseQuery(args, api, extraOptions);
+    } else {
+        console.log('no data');
+        api.dispatch(logout());
     }
     return result;
 };

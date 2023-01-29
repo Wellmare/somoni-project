3a5f1a37963c7a04dea487af23f1d8237fe2510f
Ivan <ivan.kolipov@gmail.com>
2023-01-29 12:12:33 +0300

    Add mb-4

--- frontend/src/components/profile/profile-components/Profile/Profile.tsx
+++ frontend/src/components/profile/profile-components/Profile/Profile.tsx
@@ -16,28 +16,9 @@ const Profile: FC<IProfileProps> = ({ profile }) => {
     return (
         <ProfileContext.Provider value={{ profile: enhancedProfile, posts }}>
             <div>
-                {/* <div className={s.avatar}>{photo !== null && <img src={photo} alt='profile photo' />}</div> */}
-                {/* <Avatar size={AvatarSize.large}> */}
-                {/*     <img src={photo} alt={username} /> */}
-                {/* </Avatar> */}
-                {/* {username} */}
-                {/* {bio} */}
-                {/* {withEdit && ( */}
-                {/*     <Button */}
-                {/*         color={ButtonColors.green} */}
-                {/*         size={ButtonSizes.md} */}
-                {/*         width={'100%'} */}
-                {/*         onClick={() => navigate(PathsToNavigate.EDIT_PROFILE)} */}
-                {/*     > */}
-                {/*         Edit profile */}
-                {/*     </Button> */}
-                {/* )} */}
-                {/* {posts !== null && posts.length > 0 ? ( */}
-                {/*     posts.map((post) => <Post post={post} key={post.id} />) */}
-                {/* ) : ( */}
-                {/*     <Info>Постов пока нет</Info> */}
-                {/* )} */}
-                <ProfileHeader />
+                <div className={'mb-4'}>
+                    <ProfileHeader />
+                </div>
                 <ProfilePosts />
             </div>
         </ProfileContext.Provider>

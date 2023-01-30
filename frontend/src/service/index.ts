c0da22ad9bfbe956298d4707ec70c2b3e62acfac
Ivan <ivan.kolipov@gmail.com>
2023-01-30 17:17:55 +0300

    Add preventing multiple unauthorized errors

--- frontend/src/service/index.ts
+++ frontend/src/service/index.ts
@@ -1,12 +1,17 @@
 import { BaseQueryFn, FetchArgs, fetchBaseQuery, FetchBaseQueryError } from '@reduxjs/toolkit/query';
 import { createApi } from '@reduxjs/toolkit/query/react';
 
+import { Mutex } from 'async-mutex';
+
 import { url } from '../constants/api';
 import { apiEndpoints } from '../constants/apiEndpoints';
 import { logout, setAuthTokens } from '../redux/slices/authSlice';
 import { RootState } from '../redux/store';
 import { ITokens } from '../types/redux/auth/ITokens';
 import { isTokenExpired } from '../utils/isTokenExpired';
+
+// create a new mutex
+const mutex = new Mutex();
 
 const baseQuery = fetchBaseQuery({
     baseUrl: url,
@@ -23,7 +28,10 @@ const baseQueryWithReAuth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQue
     api,
     extraOptions,
 ) => {
-    const result = await baseQuery(args, api, extraOptions);
+    // wait until the mutex is available without locking it
+    await mutex.waitForUnlock();
+
+    let result = await baseQuery(args, api, extraOptions);
     const authTokens = (api.getState() as RootState).auth?.authTokens;
 
     console.log('authTokens', authTokens);
@@ -34,51 +42,55 @@ const baseQueryWithReAuth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQue
         return result;
     }
 
-    // const isAuth = (api.getState() as RootState).auth.isAuth;
-    // const authTokens = getAuthTokensFromLocalStorage();
+    if (!mutex.isLocked()) {
+        const release = await mutex.acquire();
+        try {
+            const refreshResult = await baseQuery(
+                {
+                    url: apiEndpoints.refreshToken,
+                    method: 'POST',
+                    body: {
+                        refresh: authTokens.refresh,
+                    },
+                },
+                api,
+                extraOptions,
+            );
+            if (refreshResult.data !== null) {
+                // store the new token
+                // console.log(refreshResult);
+                const newTokens = refreshResult.data as ITokens;
 
-    // console.log('isAuth', isAuth);
-    // console.log('authTokens from state', authTokens);
-    //
-    // if (!isAuth) return result;
+                if (newTokens === undefined || !('refresh' in newTokens) || !('access' in newTokens)) {
+                    console.log('Tokens is undefined!');
+                    return result;
+                }
 
-    // if (result.error != null && result.error.status === 401 && authTokens !== null && authTokens.refresh !== '') {
-    //     // console.log('not auth');
-    //     // try to get a new token
-    //     console.log('try auth');
-    const refreshResult = await baseQuery(
-        {
-            url: apiEndpoints.refreshToken,
-            method: 'POST',
-            body: {
-                refresh: authTokens.refresh,
-            },
-        },
-        api,
-        extraOptions,
-    );
-    if (refreshResult.data !== null) {
-        // store the new token
-        console.log(refreshResult);
-        const newTokens = refreshResult.data as ITokens;
+                // setAuthTokensToLocalStorage(newTokens);
 
-        if (newTokens === undefined || !('refresh' in newTokens) || !('access' in newTokens)) {
-            console.log('Tokens is undefined!');
-            return result;
+                api.dispatch(setAuthTokens(refreshResult.data as ITokens));
+                // retry the initial query
+
+                console.log('set auth');
+                // noinspection ES6RedundantAwait
+                return await baseQuery(args, api, extraOptions);
+            } else {
+                console.log('no data');
+                await api.dispatch(logout());
+            }
+        } catch (e) {
+            console.log(e);
+            api.dispatch(logout());
+        } finally {
+            // release must be called once the mutex should be released again.
+            release();
         }
-
-        // setAuthTokensToLocalStorage(newTokens);
-
-        api.dispatch(setAuthTokens(refreshResult.data as ITokens));
-        // retry the initial query
-
-        console.log('set auth');
-        // noinspection ES6RedundantAwait
-        return await baseQuery(args, api, extraOptions);
     } else {
-        console.log('no data');
-        api.dispatch(logout());
+        // wait until the mutex is available without locking it
+        await mutex.waitForUnlock();
+        result = await baseQuery(args, api, extraOptions);
     }
+
     return result;
 };
 

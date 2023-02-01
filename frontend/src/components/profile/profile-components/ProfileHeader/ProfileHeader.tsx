8e97ddcde2360dce06ea34d1bdfa557730135879
Ivan <ivan.kolipov@gmail.com>
2023-02-01 17:11:51 +0300

    Add adaptive in ProfileHeader

--- frontend/src/components/profile/profile-components/ProfileHeader/ProfileHeader.tsx
+++ frontend/src/components/profile/profile-components/ProfileHeader/ProfileHeader.tsx
@@ -14,15 +14,17 @@ const ProfileHeader: FC = () => {
     const { username, isMyProfile } = profile;
 
     return (
-        <div className={classNames('flex', 'justify-around', 'items-start', 'w-full', 'mx-auto')}>
-            <div className={classNames('flex', 'justify-start', 'items-start')}>
-                <ProfileAvatar />
-                <div className={'ml-4'}>
-                    <h2 className={s.username}>{username}</h2>
-                    <ProfileBio />
+        <div className={classNames('mx-auto', 'w-11/12', 'sm:w-11/12', 'md:w-11/12', 'lg:w-8/12', 'xl:w-6/12')}>
+            <div className={classNames('flex', 'justify-between', 'items-start')}>
+                <div className={classNames('flex', 'justify-start', 'items-center', 'flex-col', 'sm:flex-row')}>
+                    <ProfileAvatar />
+                    <div className={'ml-4'}>
+                        <h2 className={s.username}>{username}</h2>
+                        <ProfileBio />
+                    </div>
                 </div>
+                <div className={'mt-5'}>{isMyProfile && <ProfileMenu />}</div>
             </div>
-            <div className={'mt-5'}>{isMyProfile && <ProfileMenu />}</div>
         </div>
     );
 };

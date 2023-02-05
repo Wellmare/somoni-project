880cf94183229a6ea28ce9842be762304a797a92
Ivan <ivan.kolipov@gmail.com>
2023-02-05 16:26:45 +0300

    Add searchInput to MainPage and PostsByTagPage

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,19 @@
 import React, { FC } from 'react';
 
+import SearchByTagForm from '../../components/forms/SearchByTagForm/SearchByTagForm';
 import Posts from '../../components/posts/Posts/Posts';
 
 const MainPage: FC = () => {
     return (
         <>
-            <Posts />
+            <div>
+                <div className='on-mobile'>
+                    <div className={'flex justify-center mb-5'}>
+                        <SearchByTagForm />
+                    </div>
+                </div>
+                <Posts />
+            </div>
         </>
     );
 };

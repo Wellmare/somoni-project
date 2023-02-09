03b7bb982c64190db3ff0ec9d5e17be16c240f91
Danila <morozovd0605@gmail.com>
2023-02-09 16:28:46 +0300

    add 127.0.0.1 d cors_whitelist

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -182,7 +182,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://localhost:3000'
+    'http://127.0.0.1:3000'
 )
 
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:3000', 'http://localhost:3000']

4dc672715211fd6423f5fc0b2206cc4c8e3bc7e9
Ivan <ivan.kolipov@gmail.com>
2023-02-11 11:46:15 +0300

    Add read state from location and EmailSendedModal

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,26 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+
+import { useLocation } from 'react-router-dom';
 
 import SearchByTagForm from '../../components/forms/SearchByTagForm/SearchByTagForm';
 import Posts from '../../components/posts/Posts/Posts';
+import { Paths } from '../../constants/Paths';
+import EmailSendedModal from '../../ui/modals/EmailSendedModal/EmailSendedModal';
 
 const MainPage: FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const { state } = useLocation();
+    console.log(state?.from);
+
+    useEffect(() => {
+        if (state?.from === Paths.REGISTER) {
+            setIsOpen(true);
+        }
+    }, []);
+
     return (
         <>
+            {isOpen && <EmailSendedModal isOpen={isOpen} setIsOpen={setIsOpen} />}
             <div>
                 <div className='on-mobile'>
                     <div className={'flex justify-center mb-5'}>

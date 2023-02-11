b65572a09972e54a49273ee38538d9fbeb360725
Ivan <ivan.kolipov@gmail.com>
2023-02-11 18:40:08 +0300

    Add loader sizes and put the variables in Loader.scss

--- frontend/src/ui/Loader/Loader.tsx
+++ frontend/src/ui/Loader/Loader.tsx
@@ -3,10 +3,29 @@ import React, { FC } from 'react';
 
 import s from './Loader.module.scss';
 
-const Loader: FC = () => {
+import { LoaderSize } from '../../types/UI/Loader.types';
+
+interface ILoaderProps {
+    size?: LoaderSize;
+}
+
+const Loader: FC<ILoaderProps> = ({ size = LoaderSize.md }) => {
+    let sizeClass = '';
+    switch (size) {
+        case LoaderSize.sm:
+            sizeClass = s.sm;
+            break;
+        case LoaderSize.md:
+            sizeClass = s.md;
+            break;
+        case LoaderSize.lg:
+            sizeClass = s.lg;
+            break;
+    }
+
     return (
         <div className={s.container}>
-            <div className={s.loader}>
+            <div className={classNames(s.loader, sizeClass)}>
                 <svg viewBox='0 0 120 120' version='1.1' xmlns='http://www.w3.org/2000/svg'>
                     <circle className={classNames(s.load, s.one)} cx='60' cy='60' r='40' />
                     <circle className={classNames(s.load, s.two)} cx='60' cy='60' r='40' />

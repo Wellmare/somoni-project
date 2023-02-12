d7a0d8e7f9c245ad78add2f3748543fc8f04fc7f
Ivan <ivan.kolipov@gmail.com>
2023-02-12 12:49:21 +0300

    Add EmailNotConfirmedCard

---
+++ frontend/src/ui/EmailNotConfirmedCard/EmailNotConfirmedCard.tsx
@@ -0,0 +1,28 @@
+import React, { FC, useState } from 'react';
+
+import { ButtonColors, ButtonSizes } from '../../types/UI/Button.types';
+import Button from '../Button/Button';
+import Card from '../Card/Card';
+import EmailConfirmModal from '../modals/EmailConfirmModal/EmailConfirmModal';
+
+// interface IEmailNotConfirmedCardProps {
+//
+// }
+
+const EmailNotConfirmedCard: FC /* <IEmailNotConfirmedCardProps> */ = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <>
+            <EmailConfirmModal isOpen={isOpen} setIsOpen={setIsOpen} required={false} />
+            <Card className={'flex justify-between items-center'}>
+                <h2>Ваш email не подтвержден</h2>
+                <Button color={ButtonColors.blue} size={ButtonSizes.sm} onClick={() => setIsOpen(true)}>
+                    Подтвердить
+                </Button>
+            </Card>
+        </>
+    );
+};
+
+export default EmailNotConfirmedCard;

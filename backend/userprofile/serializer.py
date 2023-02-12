fb44e584cb4ff40803442bdfa870b533a8ecabbc
Danila <morozovd0605@gmail.com>
2023-02-12 22:27:13 +0300

    смена почты и ее подтверждение

--- backend/userprofile/serializer.py
+++ backend/userprofile/serializer.py
@@ -1,3 +1,7 @@
+import os
+
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 from rest_framework import serializers
 
 from myapi.models import User
@@ -10,13 +14,25 @@ class profile_serializer(serializers.ModelSerializer):
         read_only_fields = ('id', )
 
     def update(self, instance, validated_data):
+        old_email = instance.email
         try:
-            instance.username = validated_data.get("username", instance.username)
             instance.bio = validated_data.get("bio", instance.bio)
             if validated_data['photo']:
                 instance.photo = validated_data.get("photo", instance.photo)
             instance.email = validated_data.get("email", instance.email)
             instance.save()
+
+            if old_email != instance.email:
+                instance.isEmailConfirmed = False
+                instance.save()
+                subject = f"Email confirm for {instance.username}".format(title="Some website title")
+                html_message = render_to_string('email_confirm_password.html',
+                                                {'token_username': instance.activate_key_username,
+                                                 'token_email': instance.activate_key_email,
+                                                 'domain': os.getenv('FRONT_DOMAIN')})
+                msg = EmailMultiAlternatives(subject=subject, to=[instance.email])
+                msg.attach_alternative(html_message, 'text/html')
+                msg.send()
         except KeyError:
             raise serializers.ValidationError(
                 {"detail": "some field is missing"})

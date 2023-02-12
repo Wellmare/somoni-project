3e592ad7b631369b2fea996005a51ffdeae56953
Ivan <ivan.kolipov@gmail.com>
2023-02-12 15:57:58 +0300

    Add text-wrap to Profile username

--- frontend/src/components/profile/profile-components/ProfileHeader/ProfileHeader.tsx
+++ frontend/src/components/profile/profile-components/ProfileHeader/ProfileHeader.tsx
@@ -15,10 +15,10 @@ const ProfileHeader: FC = () => {
 
     return (
         <div className={classNames('flex', 'justify-between', 'items-start')}>
-            <div className={classNames('flex', 'justify-start', 'items-center', 'flex-col', 'sm:flex-row')}>
+            <div className={classNames('flex', 'justify-start', 'items-start', 'flex-col', 'sm:flex-row')}>
                 <ProfileAvatar />
                 <div className={'ml-4'}>
-                    <h2 className={s.username}>{username}</h2>
+                    <h2 className={`${s.username} text-wrap`}>{username}</h2>
                     <ProfileBio />
                 </div>
             </div>

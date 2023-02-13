950362b5498a1282df7a10ba9ac1c47c818152fd
Ivan <ivan.kolipov@gmail.com>
2023-02-13 16:46:58 +0300

    Add link to profile on click on avatar

--- frontend/src/components/profile/profile-components/ProfileAvatar/ProfileAvatar.tsx
+++ frontend/src/components/profile/profile-components/ProfileAvatar/ProfileAvatar.tsx
@@ -1,20 +1,23 @@
 import React, { FC, useContext } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import { ProfileContext } from '../../../../context/ProfileContext';
 import { AvatarSize } from '../../../../types/UI/Avatar.types';
 import Avatar from '../../../../ui/Avatar/Avatar';
+import { pathsToNavigate } from '../../../../utils/pathsToNavigate';
 
 const ProfileAvatar: FC = () => {
     const { profile } = useContext(ProfileContext);
     if (profile === null) return null;
-    const { username, avatarLink } = profile;
+    const { username, avatarLink, profileId } = profile;
 
     return (
         <div className={'flex-shrink-0'}>
             <Avatar size={AvatarSize.large}>
-                <a href={avatarLink} target={'_blank'} rel='noreferrer'>
+                <Link to={pathsToNavigate.user(profileId)}>
                     <img src={avatarLink} alt={username} />
-                </a>
+                </Link>
             </Avatar>
         </div>
     );

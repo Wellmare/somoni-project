769e1dca2d462b791ecc3d0f97eead20fe7f6b72
Ivan <ivan.kolipov@gmail.com>
2023-02-14 16:52:04 +0300

    Fix Error styles and color

--- frontend/src/ui/Error/Error.tsx
+++ frontend/src/ui/Error/Error.tsx
@@ -5,10 +5,11 @@ import s from './Error.module.scss';
 
 interface IErrorProps {
     children: ReactNode;
+    className?: string;
 }
 
-const Error: FC<IErrorProps> = ({ children }) => {
-    return <div className={classNames(s.error)}>{children}</div>;
+const Error: FC<IErrorProps> = ({ children, className }) => {
+    return <div className={classNames(s.error, className)}>{children}</div>;
 };
 
 export default Error;

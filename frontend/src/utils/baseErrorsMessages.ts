9610272459c484725e9ab79f78babdc88c0086ed
Ivan <ivan.kolipov@gmail.com>
2023-02-15 16:59:18 +0300

    Add 500, 404, 401, 403 to baseErrorsMessages

--- frontend/src/utils/baseErrorsMessages.ts
+++ frontend/src/utils/baseErrorsMessages.ts
@@ -2,4 +2,8 @@ import { IMessageToStatusCode } from '../types/IMessageToStatusCode';
 
 export const baseErrorsMessages: IMessageToStatusCode[] = [
     { statusCode: 'FETCH_ERROR', message: 'Не удалось получить ответ от сервера' },
+    { statusCode: 500, message: 'Ошибка на сервере!' },
+    { statusCode: 404, message: 'Не найдено!' },
+    { statusCode: 401, message: 'Ошибка авторизации!' },
+    { statusCode: 403, message: 'Ошибка доступа!' },
 ];

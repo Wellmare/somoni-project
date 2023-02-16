eb526b099adfa6926440364a94a29929af1141ed
Ivan <ivan.kolipov@gmail.com>
2023-02-16 15:55:14 +0300

    Temporally: add debugger

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import React, { FC } from 'react';
 import Router from './components/routing/Router';
 
 const App: FC = () => {
+    // eslint-disable-next-line no-debugger
+    debugger;
     return (
         <>
             <Router />

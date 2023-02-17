35408dbb1630c004251673bdddedf90b7f99f47c
Ivan <ivan.kolipov@gmail.com>
2023-02-17 08:11:14 +0300

    Revert "Temporally: add debugger"
    This reverts commit eb526b099adfa6926440364a94a29929af1141ed.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,6 @@ import React, { FC } from 'react';
 import Router from './components/routing/Router';
 
 const App: FC = () => {
-    // eslint-disable-next-line no-debugger
-    debugger;
     return (
         <>
             <Router />

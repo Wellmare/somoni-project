81f7e6345903f8fbabe2a661c0ae4615cec368f5
Danila <morozovd0605@gmail.com>
2023-02-17 20:52:39 +0300

    isEmailChanged

--- backend/userprofile/views.py
+++ backend/userprofile/views.py
@@ -149,7 +149,7 @@ class edit_profile(generics.RetrieveUpdateDestroyAPIView):
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
-
+        old_email = instance.email
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
@@ -158,7 +158,13 @@ class edit_profile(generics.RetrieveUpdateDestroyAPIView):
             # If 'prefetch_related' has been applied to a queryset, we need to
             # forcibly invalidate the prefetch cache on the instance.
             instance._prefetched_objects_cache = {}
-        return Response(serializer.data)
+
+        new_email = instance.email
+        if new_email != old_email:
+            isEmailChanged = True
+        else:
+            isEmailChanged = False
+        return Response({'Profile':serializer.data, 'isEmailChanged': isEmailChanged})
 
     def perform_update(self, serializer):
         serializer.save()

bca9b6933435d9d0dcc5fccaa54826325c27552b
Ivan <ivan.kolipov@gmail.com>
2023-02-19 17:27:00 +0300

    Add notification Layout

--- frontend/src/components/notifications/Notification/Notification.tsx
+++ frontend/src/components/notifications/Notification/Notification.tsx
@@ -1,13 +1,27 @@
 import React, { FC } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import { INotification } from '../../../types/redux/notifications/INotification';
+import Card from '../../../ui/Card/Card';
+import { getLocalDateFromString } from '../../../utils/getLocalDateFromString';
 
 interface INotificationProps {
     notification: INotification;
 }
 
-const Notification: FC<INotificationProps> = ({ notification }) => {
-    return <div>{notification.text}</div>;
+const Notification: FC<INotificationProps> = ({ notification: { text, date, link } }) => {
+    const localDate = getLocalDateFromString(date);
+    return (
+        <>
+            <Link to={link}>
+                <Card className={'px-2 py-1'}>
+                    <div>{text}</div>
+                    <div className={'flex justify-end'}>{localDate}</div>
+                </Card>
+            </Link>
+        </>
+    );
 };
 
 export default Notification;

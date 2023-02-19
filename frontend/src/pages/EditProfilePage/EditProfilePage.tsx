58ca57704f3c4b0c4cf6e7ed33eb06ede372bdad
Ivan <ivan.kolipov@gmail.com>
2023-02-19 13:51:14 +0300

    Add change password and delete profile in EditProfilePage

--- frontend/src/pages/EditProfilePage/EditProfilePage.tsx
+++ frontend/src/pages/EditProfilePage/EditProfilePage.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import EditProfileForm from '../../components/forms/profile/EditProfileForm/EditProfileForm';
+import DeleteProfileAlert from '../../components/profile/profile-components/DeleteProfileAlert/DeleteProfileAlert';
 import ServerResponse from '../../components/server/ServerResponse/ServerResponse';
+import { PathsToNavigate } from '../../constants/Paths';
 import { useGetDataToEditUserQuery } from '../../service/userApiSlice';
+import { ButtonColors, ButtonSizes } from '../../types/UI/Button.types';
+import Button from '../../ui/Button/Button';
 
 const EditProfilePage: FC = () => {
     const { data, error, isError, isLoading, isSuccess } = useGetDataToEditUserQuery(undefined);
@@ -19,6 +25,20 @@ const EditProfilePage: FC = () => {
                         id={data.id != null ? data.id : '1'}
                     />
                 )}
+                <div className={'flex justify-center items-center flex-col mt-6 mx-auto'} style={{ width: '400px' }}>
+                    <Link to={PathsToNavigate.CHANGE_PASSWORD} className={'w-full mb-4'}>
+                        <Button color={ButtonColors.blue} size={ButtonSizes.sm} className={'w-full'}>
+                            Изменить пароль
+                        </Button>
+                    </Link>
+                    <div className={'w-full'}>
+                        <DeleteProfileAlert>
+                            <Button color={ButtonColors.red} size={ButtonSizes.sm} className={'w-full'}>
+                                Удалить профиль
+                            </Button>
+                        </DeleteProfileAlert>
+                    </div>
+                </div>
             </ServerResponse>
         </>
     );

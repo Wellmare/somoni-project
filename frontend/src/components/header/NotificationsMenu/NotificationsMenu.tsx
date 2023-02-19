57117edae3125f20d5e9645d0585d5e8b2bec676
Ivan <ivan.kolipov@gmail.com>
2023-02-19 17:14:24 +0300

    Add badge to NotificationsMenu

--- frontend/src/components/header/NotificationsMenu/NotificationsMenu.tsx
+++ frontend/src/components/header/NotificationsMenu/NotificationsMenu.tsx
@@ -3,6 +3,8 @@ import React, { FC } from 'react';
 import s from './NotificationsMenu.module.scss';
 
 import { ReactComponent as NotificationIcon } from '../../../assets/svg/notification.svg';
+import { BadgeColor } from '../../../types/UI/Badge.types';
+import Badge from '../../../ui/Badge/Badge';
 import Icon from '../../../ui/Icon/Icon';
 import Menu from '../../Menu/Menu';
 import Notifications from '../../notifications/Notifications/Notifications';
@@ -14,15 +16,17 @@ const NotificationsMenu: FC /* <INotificationMenuProps> */ = () => {
         <Menu
             menuButton={
                 <>
-                    <div className={'cursor-pointer h-full flex items-center justify-center mr-3'}>
+                    <div className={'cursor-pointer h-full flex items-center justify-center mr-3 relative'}>
                         <Icon customTypeClassName={s.icon}>
                             <NotificationIcon />
                         </Icon>
+                        <Badge color={BadgeColor.red}>1</Badge>
                     </div>
                 </>
             }
         >
             <div className={s.wrapper}>
+                <h2>Уведомления: </h2>
                 <Notifications />
             </div>
         </Menu>

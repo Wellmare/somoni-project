872433d29e6dbdb47c19b826bd5b7246bd336f5c
Ivan <ivan.kolipov@gmail.com>
2023-02-19 13:52:16 +0300

    Add count followers, following to enhanceIProfileServerResponse

--- frontend/src/utils/enhanceIProfileServerResponse.ts
+++ frontend/src/utils/enhanceIProfileServerResponse.ts
@@ -7,7 +7,20 @@ import { IProfile } from '../types/redux/profile/IProfile';
 export const enhanceIProfileServerResponse = (
     profileServerResponse: IPaginatedProfileResponse,
 ): { profile: IProfile; posts: IPost[] | null } => {
-    const { results, isMyProfile, bio, id, email, username, photo, isEmailConfirmed } = profileServerResponse;
+    const {
+        results,
+        isMyProfile,
+        bio,
+        id,
+        email,
+        username,
+        photo,
+        isEmailConfirmed,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        count_followers,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        count_following,
+    } = profileServerResponse;
 
     const enhancedProfile: IProfile = {
         isMyProfile,
@@ -17,6 +30,8 @@ export const enhanceIProfileServerResponse = (
         bio,
         avatarLink: photo,
         isEmailConfirmed,
+        countFollowers: count_followers,
+        countFollowing: count_following,
     };
     if (results === null) {
         return { posts: null, profile: enhancedProfile };

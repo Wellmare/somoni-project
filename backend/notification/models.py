c6a88f8316dc6447aac16dc8f4adc364c9662f3c
Danila <morozovd0605@gmail.com>
2023-02-20 20:56:22 +0300

    новая таблица в бд

--- backend/notification/models.py
+++ backend/notification/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+from myapi.models import User
+
+class Notification(models.Model):
+    html = models.CharField(max_length=500)
+    date = models.DateTimeField(default=timezone.now)
+    isRead = models.BooleanField(default=False)
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE)

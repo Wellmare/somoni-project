9ca1696c8f09e8cf190fdac4b2e3d0e49a367090
Danila <morozovd0605@gmail.com>
2023-02-20 20:56:05 +0300

    настройка приложения

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'notification'
+    'notification',
     'myapi',
     'password_reset',
     'rest_framework',
--- backend/notification/admin.py
+++ backend/notification/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from notification.models import Notification
+
+admin.site.register(Notification)

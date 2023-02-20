613427639070a901c55bac16334738b84c25bc8f
Ivan <ivan.kolipov@gmail.com>
2023-02-20 17:52:59 +0300

    Remove Link and add SanitizeHTML

--- frontend/src/components/notifications/Notification/Notification.tsx
+++ frontend/src/components/notifications/Notification/Notification.tsx
@@ -1,25 +1,26 @@
 import React, { FC } from 'react';
 
-import { Link } from 'react-router-dom';
+import s from './Notification.module.scss';
 
 import { INotification } from '../../../types/redux/notifications/INotification';
 import Card from '../../../ui/Card/Card';
 import { getLocalDateFromString } from '../../../utils/getLocalDateFromString';
+import SanitizeHTML from '../../SanitizeHtml/SanitizeHtml';
 
 interface INotificationProps {
     notification: INotification;
 }
 
-const Notification: FC<INotificationProps> = ({ notification: { text, date, link } }) => {
+const Notification: FC<INotificationProps> = ({ notification: { html, date, id, isRead } }) => {
     const localDate = getLocalDateFromString(date);
     return (
         <>
-            <Link to={link}>
-                <Card className={'px-2 py-1'}>
-                    <div>{text}</div>
-                    <div className={'flex justify-end'}>{localDate}</div>
-                </Card>
-            </Link>
+            {/* <Link to={link}> */}
+            <Card className={`px-2 py-1 ${s.notification}`}>
+                <SanitizeHTML html={html} />
+                <div className={'flex justify-end mt-1'}>{localDate}</div>
+            </Card>
+            {/* </Link> */}
         </>
     );
 };

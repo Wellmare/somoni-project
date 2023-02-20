b1b7c3c95c045d5d53df5126ad4e85d4d651d668
Danila <morozovd0605@gmail.com>
2023-02-20 19:22:59 +0300

    create app notification

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'notification'
     'myapi',
     'password_reset',
     'rest_framework',

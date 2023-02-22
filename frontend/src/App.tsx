3ab88be27852898e5f84b5da90e652746d2c96e9
Ivan <ivan.kolipov@gmail.com>
2023-02-22 20:48:57 +0300

    Add getLastNotifications in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,29 +3,40 @@ import React, { FC, useEffect } from 'react';
 import Router from './components/routing/Router';
 import { useAppDispatch, useAppSelector } from './hooks/reduxHooks';
 import { selectIsAuth } from './redux/slices/authSlice';
-import { addNotification } from './redux/slices/notificationsSlice';
+import { addNotification, setNotifications } from './redux/slices/notificationsSlice';
 import { connectToNotifications } from './service/connectToNotifications';
+import { useLazyGetLastNotificationsQuery } from './service/notificationsApiSlice';
+import { doAsyncFunc } from './utils/doAsyncFunc';
 
 const App: FC = () => {
     const dispatch = useAppDispatch();
     const isAuth = useAppSelector(selectIsAuth);
+    const [getLastNotifications] = useLazyGetLastNotificationsQuery();
 
-    // useEffect(() => {
-    //     if (isAuth) {
-    //         const eventSource = connectToNotifications(
-    //             (event) => {
-    //                 console.log('connect to notifications');
-    //             },
-    //             (notification) => {
-    //                 console.log(notification);
-    //                 dispatch(addNotification(notification));
-    //             },
-    //             (event) => {
-    //                 console.error('Connection error');
-    //             },
-    //         );
-    //     }
-    // }, []);
+    useEffect(() => {
+        if (isAuth) {
+            try {
+                doAsyncFunc(async () => {
+                    const notifications = await getLastNotifications(undefined);
+                    dispatch(setNotifications(notifications.data));
+                    const eventSource = connectToNotifications(
+                        (event) => {
+                            console.log('connect to notifications');
+                        },
+                        (notification) => {
+                            console.log(notification);
+                            dispatch(addNotification(notification));
+                        },
+                        (event) => {
+                            console.error('Connection error');
+                        },
+                    );
+                });
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    }, []);
 
     return (
         <>

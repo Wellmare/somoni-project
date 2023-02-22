14a2763685dc9b79d2bb064dad1b968e496fda39
Ivan <ivan.kolipov@gmail.com>
2023-02-22 15:02:27 +0300

    Add notificationsApiSlice

---
+++ frontend/src/service/notificationsApiSlice.ts
@@ -0,0 +1,38 @@
+import { apiSlice } from './index';
+
+import { apiEndpoints } from '../constants/apiEndpoints';
+import { IDataToReadNotification } from '../types/redux/notifications/IDataTo';
+
+export const notificationsApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getAllNotifications: builder.query({
+            query: () => ({
+                url: apiEndpoints.getAllNotifications,
+            }),
+        }),
+        getLastNotifications: builder.query({
+            query: () => ({
+                url: apiEndpoints.getLastNotifications,
+            }),
+        }),
+        readNotification: builder.mutation<undefined, IDataToReadNotification>({
+            query: ({ id }) => ({
+                url: apiEndpoints.readNotification(id),
+                method: 'POST',
+            }),
+        }),
+        readAllNotifications: builder.mutation<undefined, undefined>({
+            query: () => ({
+                url: apiEndpoints.readAllNotification,
+                method: 'POST',
+            }),
+        }),
+    }),
+});
+
+export const {
+    useGetAllNotificationsQuery,
+    useGetLastNotificationsQuery,
+    useReadAllNotificationsMutation,
+    useReadNotificationMutation,
+} = notificationsApiSlice;

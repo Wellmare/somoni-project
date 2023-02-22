0be6119c5ea16265684b8f3a39cea6c392c84188
Ivan <ivan.kolipov@gmail.com>
2023-02-22 15:03:11 +0300

    Add notificationsSlice

--- frontend/src/redux/slices/notificationsSlice.ts
+++ frontend/src/redux/slices/notificationsSlice.ts
@@ -5,7 +5,20 @@ import { INotificationsSlice } from '../../types/redux/notifications/INotificati
 import { RootState } from '../store';
 
 const initialState: INotificationsSlice = {
-    notifications: [],
+    notifications: [
+        {
+            html: '<a href="https://somoni.org/user/50">test_regru</a> поставил лайк под пост <a href="https://somoni.org/post/12">Я купил наушники</a>',
+            date: '2023-02-19T16:40:51.532181+03:00',
+            id: 'asd',
+            isRead: false,
+        },
+        {
+            html: '<a href="https://somoni.org/user/50">test_regru</a> оставил комментарий под пост <a href="https://somoni.org/post/12">Я купил наушники</a>',
+            date: '2023-02-19T16:40:51.532181+03:10',
+            id: 'asdasdsdsd',
+            isRead: false,
+        },
+    ],
 };
 
 export const notificationsSlice = createSlice({
@@ -18,9 +31,23 @@ export const notificationsSlice = createSlice({
         addNotification: (state, action: PayloadAction<INotification>) => {
             state.notifications.push(action.payload);
         },
+        setIsReadInState: (state, { payload: { isRead, id } }: PayloadAction<{ id: string; isRead: boolean }>) => {
+            state.notifications = state.notifications.map((notification) => {
+                if (notification.id === id) {
+                    return { ...notification, isRead };
+                }
+                return notification;
+            });
+        },
+        readAllNotificationsInState: (state, action) => {
+            state.notifications = state.notifications.map((notification) => {
+                return { ...notification, isRead: true };
+            });
+        },
     },
 });
 
 export const notificationsSliceReducer = notificationsSlice.reducer;
-export const { setNotifications, addNotification } = notificationsSlice.actions;
+export const { setNotifications, addNotification, setIsReadInState, readAllNotificationsInState } =
+    notificationsSlice.actions;
 export const selectNotifications = (state: RootState): INotification[] => state.notifications.notifications;

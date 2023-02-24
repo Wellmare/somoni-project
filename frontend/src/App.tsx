413d92b5e813d1a9e3c32d52f9530fdacd8f07f7
Ivan <ivan.kolipov@gmail.com>
2023-02-24 20:14:55 +0300

    Add sendNotification to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import React, { FC, useEffect } from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
 import Router from './components/routing/Router';
 import { useAppDispatch, useAppSelector } from './hooks/reduxHooks';
 import { selectIsAuth, selectUserId } from './redux/slices/authSlice';
@@ -7,14 +9,23 @@ import { addNotification, setNotifications } from './redux/slices/notificationsS
 import { connectToNotifications } from './service/connectToNotifications';
 import { useLazyGetLastNotificationsQuery } from './service/notificationsApiSlice';
 import { doAsyncFunc } from './utils/doAsyncFunc';
+import { htmlToPlainText } from './utils/htmlToPlainText';
+import { sendNotification } from './utils/sendNotification';
 
 const App: FC = () => {
     const dispatch = useAppDispatch();
     const isAuth = useAppSelector(selectIsAuth);
     const userId = useAppSelector(selectUserId);
     const [getLastNotifications] = useLazyGetLastNotificationsQuery();
+    const navigate = useNavigate();
 
     useEffect(() => {
+        setTimeout(() => {
+            sendNotification(htmlToPlainText('<a href="https://sa">Пользователь</a> прислал уведомление'), () =>
+                navigate('/users/12', { relative: 'route' }),
+            );
+        }, 100);
+
         if (isAuth && userId != null) {
             try {
                 doAsyncFunc(async () => {
@@ -28,6 +39,7 @@ const App: FC = () => {
                         (notification) => {
                             console.log(notification);
                             dispatch(addNotification(notification));
+                            sendNotification(htmlToPlainText(notification.html), () => navigate(notification.mainLink));
                         },
                         (event) => {
                             console.error('Connection error');
